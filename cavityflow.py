aa8627a42d56e2c615aa720a17c8661530f51402
Syed Hussain Ather <shussainather@gmail.com>
2019-02-13 01:17:35 -0500

    more math

--- cavityflow.py
+++ cavityflow.py
@@ -98,3 +98,44 @@ def cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu):
     
         
     return u, v, p
+
+u = numpy.zeros((ny, nx))
+v = numpy.zeros((ny, nx))
+p = numpy.zeros((ny, nx))
+b = numpy.zeros((ny, nx))
+nt = 100
+u, v, p = cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu)
+
+fig = pyplot.figure(figsize=(11,7), dpi=100)
+# plotting the pressure field as a contour
+pyplot.contourf(X, Y, p, alpha=0.5, cmap=cm.viridis)
+pyplot.colorbar()
+# plotting the pressure field outlines
+pyplot.contour(X, Y, p, cmap=cm.viridis)
+# plotting velocity field
+pyplot.quiver(X[::2, ::2], Y[::2, ::2], u[::2, ::2], v[::2, ::2])
+pyplot.xlabel('X')
+pyplot.ylabel('Y');
+
+u = numpy.zeros((ny, nx))
+v = numpy.zeros((ny, nx))
+p = numpy.zeros((ny, nx))
+b = numpy.zeros((ny, nx))
+nt = 700
+u, v, p = cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu)
+
+fig = pyplot.figure(figsize=(11, 7), dpi=100)
+pyplot.contourf(X, Y, p, alpha=0.5, cmap=cm.viridis)
+pyplot.colorbar()
+pyplot.contour(X, Y, p, cmap=cm.viridis)
+pyplot.quiver(X[::2, ::2], Y[::2, ::2], u[::2, ::2], v[::2, ::2])
+pyplot.xlabel('X')
+pyplot.ylabel('Y');
+
+fig = pyplot.figure(figsize=(11, 7), dpi=100)
+pyplot.contourf(X, Y, p, alpha=0.5, cmap=cm.viridis)
+pyplot.colorbar()
+pyplot.contour(X, Y, p, cmap=cm.viridis)
+pyplot.streamplot(X, Y, u, v)
+pyplot.xlabel('X')
+pyplot.ylabel('Y');
