51f444328a4ac3c867ed95abb7fa39030fb628c1
Syed Hussain Ather <shussainather@gmail.com>
2019-02-15 00:10:28 -0500

    fixed syntax

--- equilibrium.py
+++ equilibrium.py
@@ -5,7 +5,7 @@ import numpy as np
 for i in range(1,nstep):
 r = rand(1)
 if (r<n[i-1]/N):
-n[i] = n[i-1] - 1 # Move atom from left to right else:
-n[i] = n[i-1] + 1 # Move atom from right to left plot(range(0,nstep),n/N)
-xlabel(’t’),ylabel(’n/N’)
-show()
+    n[i] = n[i-1] - 1 # Move atom from left to right else:
+    n[i] = n[i-1] + 1 # Move atom from right to left
+    plt(range(0,nstep),n/N, xlabel="t", ylabel="n/N")
+    plt.show()
