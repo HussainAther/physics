182aa37ac335ee41dd4a928254d1c3a4e754430c
Syed Hussain Ather <shussainather@gmail.com>
2019-02-19 15:41:15 -0500

    folders

--- nlinfitsample1.m
+++
@@ -1,6 +0,0 @@
-S = load('reaction');
-X = S.reactants;
-y = S.rate;
-beta0 = S.beta;
-
-beta = nlinfit(X,y,@hougen,beta0)
--- nlinsample.m
+++
@@ -1,4 +0,0 @@
-S = load('reaction');
-X = S.reactants;
-y = S.rate;
-beta0 = S.beta;
