743fe2222fc2e85c1c9958aaffeb475ed8f92737
Syed Hussain Ather <shussainather@gmail.com>
2019-02-18 20:11:53 -0500

    begin class

--- sho.py
+++ sho.py
@@ -3,5 +3,20 @@ from sympy.functions.special.tensor_functions import KroneckerDelta
 import sympy.physics.quantum as sm
 
 """
-Simple Harmonic Oscillator 1-Dimension
+Simple Harmonic Oscillator in 1-Dimension
 """
+class SHOOp(Operator):
+    """
+    SHO Operators
+    """
+    @classmethod
+    def _eval_args(cls, args):
+        args = QExpr._Eval_args(args)
+        if len(args) == 1:
+            return args
+        else:
+            raise ValueError("Only one argument")
+
+    @classmethod
+    def _eval_hilbert_space(cls, label):
+        return sm.hilbert.ComplexSpace(S.Infinity)
