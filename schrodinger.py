9bf5b0d51dc36a8194df53d75581957df0e14fe3
Syed Hussain Ather <shussainather@gmail.com>
2019-02-19 12:18:52 -0500

    initial commit

---
+++ schrodinger.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+In 1926, Erwin Schrodinger advanced the famous wave equation that relates the energy of a system to its wave properties.
+Because its application to the hydrogen atom is rather complicated, we shall first use wave equation to solve the particle-in-a-box. The Schrodinger Wave equation expressing in 1D is
+"""
+
+
+#Constants
+h = 6.626e-34
+m = 9.11e-31
+#Values for L and x
+x_list = np.linspace(0,1,100)
+L = 1
+def psi(n,L,x):
+    return np.sqrt(2/L)*np.sin(n*np.pi*x/L)
+def psi_2(n,L,x):
+    return np.square(psi(n,L,x))
+plt.figure(figsize=(15,10))
+plt.suptitle("Wave Functions", fontsize=18)
+for n in range(1,4):
+    #Empty lists for energy and psi wave
+    psi_2_list = []
+    psi_list = []
+    for x in x_list:
+        psi_2_list.append(psi_2(n,L,x))
+        psi_list.append(psi(n,L,x))
+    plt.subplot(3,2,2*n-1)
+    plt.plot(x_list, psi_list)
+    plt.xlabel("L", fontsize=13)
+    plt.ylabel("Ψ", fontsize=13)
+    plt.xticks(np.arange(0, 1, step=0.5))
+    plt.title("n="+str(n), fontsize=16)
+    plt.grid()
+    plt.subplot(3,2,2*n)
+    plt.plot(x_list, psi_2_list)
+    plt.xlabel("L", fontsize=13)
+    plt.ylabel("Ψ*Ψ", fontsize=13)
+    plt.xticks(np.arange(0, 1, step=0.5))
+    plt.title("n="+str(n), fontsize=16)
+    plt.grid()
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
