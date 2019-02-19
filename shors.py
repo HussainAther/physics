54dff946372262e6913ed4e36ca4a96858b10210
Syed Hussain Ather <shussainather@gmail.com>
2019-02-19 12:47:01 -0500

    QuantumState class

--- shors.py
+++ shors.py
@@ -10,3 +10,9 @@ class Mapping:
     def __init__(self, state, amplitude):
         self.state = state
         self.amplitude = amplitude
+
+class QuantumState:
+    def __init__(self, amplitude, register):
+        self.amplitude = amplitude
+        self.register = register
+        self.entangled = {}
