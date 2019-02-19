16ead31cafe4c28353d02563f022266d6e6af1ff
Syed Hussain Ather <shussainather@gmail.com>
2019-02-19 18:54:58 -0500

    all done

--- quantum/shors.py
+++ quantum/shors.py
@@ -285,3 +285,161 @@ def extendedGCD(a, b):
         b = tA
 
     return fractions
+
+
+# Continued Fractions
+def cf(y, Q, N):
+    fractions = extendedGCD(y, Q)
+    depth = 2
+
+    def partial(fractions, depth):
+        c = 0
+        r = 1
+
+        for i in reversed(range(depth)):
+            tR = fractions[i] * r + c
+            c = r
+            r = tR
+
+        return c
+
+    r = 0
+    for d in range(depth, len(fractions) + 1):
+        tR = partial(fractions, d)
+        if tR == r or tR >= N:
+            return r
+
+        r = tR
+
+    return r
+
+# Modular Exponentiation
+def modExp(a, exp, mod):
+    fx = 1
+    while exp > 0:
+        if (exp & 1) == 1:
+            fx = fx * a % mod
+        a = (a * a) % mod
+        exp = exp >> 1
+
+    return fx
+
+def pick(N):
+    a = math.floor((random.random() * (N - 1)) + 0.5)
+    return a
+
+def checkCandidates(a, r, N, neighborhood):
+    if r is None:
+        return None
+
+    # Check multiples
+    for k in range(1, neighborhood + 2):
+        tR = k * r
+        if modExp(a, a, N) == modExp(a, a + tR, N):
+            return tR
+
+    # Check lower neighborhood
+    for tR in range(r - neighborhood, r):
+        if modExp(a, a, N) == modExp(a, a + tR, N):
+            return tR
+
+    # Check upper neigborhood
+    for tR in range(r + 1, r + neighborhood + 1):
+        if modExp(a, a, N) == modExp(a, a + tR, N):
+            return tR
+
+    return None
+
+def shors(N, attempts = 1, neighborhood = 0.0, numPeriods = 1):
+    if(N.bit_length() > BIT_LIMIT or N < 3):
+        return False
+
+    periods = []
+    neighborhood = math.floor(N * neighborhood) + 1
+
+    printInfo("N = " + str(N))
+    printInfo("Neighborhood = " + str(neighborhood))
+    printInfo("Number of periods = " + str(numPeriods))
+
+    for attempt in range(attempts):
+        printInfo("\nAttempt #" + str(attempt))
+
+        a = pick(N)
+        while a < 2:
+            a = pick(N)
+
+        d = gcd(a, N)
+        if d > 1:
+            printInfo("Found factors classically, re-attempt")
+            continue
+
+        r = findPeriod(a, N)
+
+        printInfo("Checking candidate period, nearby values, and multiples")
+
+        r = checkCandidates(a, r, N, neighborhood)
+
+        if r is None:
+            printInfo("Period was not found, re-attempt")
+            continue
+
+        if (r % 2) > 0:
+            printInfo("Period was odd, re-attempt")
+            continue
+
+        d = modExp(a, (r // 2), N)
+        if r == 0 or d == (N - 1):
+            printInfo("Period was trivial, re-attempt")
+            continue
+
+        printInfo("Period found\tr = " + str(r))
+
+        periods.append(r)
+        if(len(periods) < numPeriods):
+            continue
+
+        printInfo("\nFinding least common multiple of all periods")
+
+        r = 1
+        for period in periods:
+            d = gcd(period, r)
+            r = (r * period) // d
+
+        b = modExp(a, (r // 2), N)
+        f1 = gcd(N, b + 1)
+        f2 = gcd(N, b - 1)
+
+        return [f1, f2]
+
+    return None
+
+####################################################################################################
+#
+#                                    Command-line functionality
+#
+####################################################################################################
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description='Simulate Shor\'s algorithm for N.')
+    parser.add_argument('-a', '--attempts', type=int, default=20, help='Number of quantum attemtps to perform')
+    parser.add_argument('-n', '--neighborhood', type=float, default=0.01, help='Neighborhood size for checking candidates (as percentage of N)')
+    parser.add_argument('-p', '--periods', type=int, default=2, help='Number of periods to get before determining least common multiple')
+    parser.add_argument('-v', '--verbose', type=bool, default=True, help='Verbose')
+    parser.add_argument('N', type=int, help='The integer to factor')
+    return parser.parse_args()
+
+def main():
+    args = parseArgs()
+
+    global printInfo
+    if args.verbose:
+        printInfo = printVerbose
+    else:
+        printInfo = printNone
+
+    factors = shors(args.N, args.attempts, args.neighborhood, args.periods)
+    if factors is not None:
+        print("Factors:\t" + str(factors[0]) + ", " + str(factors[1]))
+
+if __name__ == "__main__":
+    main()
