c5852e67fda9cc631868a8636a9757ef1d761022
Syed Hussain Ather <shussainather@gmail.com>
2019-02-19 12:40:16 -0500

    Mapping class

--- shors.py
+++ shors.py
@@ -5,3 +5,8 @@ import argparse
 """
 Shor's algorithm for quantum integer factorization
 """
+
+class Mapping:
+    def __init__(self, state, amplitude):
+        self.state = state
+        self.amplitude = amplitude
