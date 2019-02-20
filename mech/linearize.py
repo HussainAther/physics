a1f91e48a67a5b22cdc1301eb73aad1409a00bc3
Syed Hussain Ather <shussainather@gmail.com>
2019-02-20 14:48:06 -0500

    frame and points

--- mech/linearize.py
+++ mech/linearize.py
@@ -1,6 +1,29 @@
 from __future__ import division
 from sympy import *
+from sympy.physics.mechanics import *
 
-x, y, z, t = symbols('x y z t')
-k, m, n = symbols('k m n', integer=True)
-f, g, h = symbols('f g h', cls=Function)
+"""
+Initialize KanesMethod object and use kanes_equation class method linearization.
+"""
+
+q1 = dynamicsymbols("q1") # Pendulum angle
+u1 = dynamicsymbols("u1") # Anguluar velocity
+q1d = dynamicsymbols("q1", 1)
+L, m, t, g = symbols("L, m, t, g")
+
+# World frame
+N = ReferenceFrame("N")
+pN = Point("N*")
+pN.set_vel(N, 0)
+
+# A.x is along the pendulum
+A = N.orientview("A", "axis", [q1, N.z])
+A.set_ang_vel(N, u1*N.z)
+
+# Locate point P relative to origin N*
+P = pN.locatenew("P", L*A.x)
+vel_P = P.v2pt_theory(pN, N, A)
+pP = Particle("pP", P, m)
+
+# Create Kinematic Differential Equations
+kde = Matrix([q1d - u1])
