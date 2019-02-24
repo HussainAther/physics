bf506dde2968deed43e17ab42547f65aef707e7d
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 12:51:51 -0500

    path class

--- mech/baton.py
+++ mech/baton.py
@@ -1,6 +1,8 @@
 from visual.graph import *
+import math
 
 class Ball:
+    """It's ball, yo.""""
     def __init__(self, mass, radius):
         self.m = mass
         self.r = radius
@@ -13,3 +15,9 @@ class Ball:
 
     def getI1(self): # moment of inertia
         return (2.0/5.0)*self.m*(self.r)**2
+
+class Path:
+    """The path the ball takes."""
+        def __init__(self, v0, theta):
+            self.g = 9.8 # acceleration due to gravity
+            self.v0 = v0 # initail velocity
