14193c75d449f691cd23e374182c672de160d574
Syed Hussain Ather <shussainather@gmail.com>
2019-02-23 21:58:50 -0500

    graph

--- mech/3drandomwalk.py
+++ mech/3drandomwalk.py
@@ -8,3 +8,5 @@ Produce a 3-D graph of 3-D Random walk
 random.seed(123)
 jmax = 1000
 xx = yy = zz = 0.0
+
+graph1 = display(x=0, y=0, width=600, height=600, title="3D Random Walk", forward=(-.6, -.5, -1))
