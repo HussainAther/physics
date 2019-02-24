c53a5a96f11c143b7fe8d1873f0b515b6ba4c7d8
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 12:54:25 -0500

    path class finish

--- mech/baton.py
+++ mech/baton.py
@@ -21,3 +21,15 @@ class Path:
         def __init__(self, v0, theta):
             self.g = 9.8 # acceleration due to gravity
             self.v0 = v0 # initail velocity
+            self.theta = theta # intial angle of trajectory
+            self.v0x = self.v0*math.cos(self.theta*math.pi/180) # velocity in x direction
+            self.v0y = self.v0*math.sin(self.theta*math.pi/180) # velocity in y direction
+
+        def getX(self, t): # x position at time t
+            self.t = t
+            return self.v0x*self.t
+
+        def getY(self, t): # y position at time t
+            self.t = t
+            return self.v0y*self.t - .5*self.g*t**2
+
