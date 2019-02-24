faf7bf8ee74d03abdd2e602531dae704fb282f27
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 18:12:30 -0500

    setup

--- mech/newton2massstring.py
+++ mech/newton2massstring.py
@@ -4,3 +4,7 @@ from vpython.graph import *
 """
 Use the Newton-Raphson search to solve the two-mass-on-a-string problem
 """
+
+scene = display(x=0, y=0, width=500, height=500, title="String and masses configuration")
+
+tempe = curve(x=range(0, 500), color=color.black)
