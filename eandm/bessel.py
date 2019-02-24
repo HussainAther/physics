e3e6ed0562e9fda3e3208d1cabebdbd951ad66c0
Syed Hussain Ather <shussainather@gmail.com>
2019-02-23 22:47:03 -0500

    finished

--- eandm/bessel.py
+++ eandm/bessel.py
@@ -16,4 +16,11 @@ funct2 = gcurve(color=color.green)
 def down(x, n, m): # Recursion woo!
     j = zeros((start + 2), float)
     j[m+1] = j[m] = 1
+    for k in range(m, 0, 01):
+        j[k-1] = ((2*k+1)/x)*j[k] - j[k+1]
+    scale = (sim(x)/x)/j[0]
+    return j[n] *scale
     
+for x in range(Xmin, Xmax, step):
+    funct1.plot(pos=(x, down(x, order, start)))
+    funct2.plot(pos=(x, down(x,1,start)))
