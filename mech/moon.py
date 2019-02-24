22713f31184784e0ad8ce7d851e90f53eed059d9
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 12:41:54 -0500

    function

--- mech/moon.py
+++ mech/moon.py
@@ -4,3 +4,10 @@ from visual.graph import *
 Moon orbiting a planet
 """
 
+graph = gdisplay(x=0, y=0, width=500, height=500, title="Motion of a satellite around a planet",
+    xtitle="x", ytitle="y", xmax=5, xmin=-5, ymax=5, ymin=-5,
+    foreground=color.black, background=color.white)
+
+moonfunction = gcurve(color=color.red)
+radius = 4 # orbit radius
+wplanet = 2 #planet angular velocity
