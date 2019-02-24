cda0352bec5bc40a467192c8dedaa25114f363d3
Syed Hussain Ather <shussainather@gmail.com>
2019-02-23 23:15:43 -0500

    finish

--- quantum/schrodingeranimated.py
+++ quantum/schrodingeranimated.py
@@ -12,4 +12,15 @@ dt = dx2/20.0
 xmax = 6.0
 
 g = display(width=500, height=250, title="Wave packet in harmonic oscillator potential")
-PlotObj = curve
+PlotObj = curve(x=xs, color=color.yellow, radius=0.1)
+g.center=(0,2,0)
+
+psr = exp(-.5*(xs/.5)**2) * cos(k0*xs)
+psi = exp(-.5*(xs/.5)**2) * sin(k0*xs)
+v = 15.0*xs**2
+
+while True:
+    rate(500)
+    psr[1:-1] = psr[1:-1] - (dt/dx2)*(psi[2:]+psi[:-2]-2*psi[1:-1]) + dt*v[1:-1]*psi[1:-1]
+    psi[1:-1] = psi[1:-1] - (dt/dx2)*(psr[2:]+psr[:-2]-2*psr[1:-1]) + dt*v[1:-1]*psr[1:-1]
+    PlotObj.y = 4*(psr**2 + psi**2)
