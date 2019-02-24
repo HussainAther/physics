d42aead21a89895f9e61e60972adc2561f0f8a0f
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 18:14:04 -0500

    comment and setup

--- mech/newton2massstring.py
+++ mech/newton2massstring.py
@@ -2,9 +2,18 @@ from numpy.linalg import solve
 from vpython.graph import *
 
 """
-Use the Newton-Raphson search to solve the two-mass-on-a-string problem
+Use the Newton-Raphson search to solve the two-mass-on-a-string problem.
+We set up matrices using coupled linear equations and check the physical reasonableness
+of them by a variety of weights and lengths. We need to check that the tensions we calculate are positive
+and that the deduced angles correspond to a physical geometry. 
 """
 
 scene = display(x=0, y=0, width=500, height=500, title="String and masses configuration")
 
 tempe = curve(x=range(0, 500), color=color.black)
+
+n = 9
+eps = 1e-6 # precision
+deriv = zeros((n,n), float) # just get some zeros
+f = zeros(n, float)
+x = array([.5, .5, .5 .5])
