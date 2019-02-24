42ab3d525a02a04d4718d4a65329adf3c81dd86d
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 12:49:39 -0500

    initial commit

---
+++ mech/baton.py
@@ -0,0 +1,12 @@
+from visual.graph import *
+
+class Ball:
+    def __init__(self, mass, radius):
+        self.m = mass
+        self.r = radius
+
+    def getM1(self): # ball mass
+        retrun self.m
+
+    def getR(self): # ball radius
+        retrun self.r
