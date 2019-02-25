7ce2fb59cd7f96c75f236aed3847f3aed644924f
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 19:37:51 -0500

    numerov alg

--- quantum/numerov.py
+++ quantum/numerov.py
@@ -13,10 +13,10 @@ energr = display(x=0, y=500, width=600, height=200, title="Potential and E")
 poten = curve(x=list(range(0, 1000)), color=color.cyan, display=energr)
 autoen = curve(x=list(range0, 1000)), display=energr)
 
-d1 = 1e-6 # interval to stop bisection
-u1 = zeros([1501], float)
-ur = zeros([1501], float)
-k21 = zeros([1501], float) # k**2 Schrodinger equation left wavefunction
+dl = 1e-6 # interval to stop bisection
+ul = zeros([1501], float) # u value for left side
+ur = zeros([1501], float) # and the right side
+k2l = zeros([1501], float) # k**2 Schrodinger equation left wavefunction
 k2r = zeros([1501], float) # k**2 S. E. right wavefunction
 n = 1501
 m = 5 # plot every 5 points
@@ -43,3 +43,14 @@ def V(x): # Finite square well from particle-in-a-box
     else:
         v = 0
     return v
+
+def setk2():
+    for i in range(0, n):
+        xl = xl0 + i*h
+        xr = xr0 - i*h
+        k2l[i] = e-V(xl)
+        k2r[i] = e-V(xr)
+
+def numerov(n, h, k2, u): # Numerov algorithm for left and right wavefunctions
+    b =
+
