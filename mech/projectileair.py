b5079de7800d44b98a6abfc79e0c94209eebbc1c
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 19:59:03 -0500

    begin variables

--- mech/projectileair.py
+++ mech/projectileair.py
@@ -1,4 +1,13 @@
+from vpython.graph import *
+
 """
 Solve for projectile motion with air resistance and
 find the frictionless case analytically.
 """
+
+v0 = 22
+angle = 34
+g = 9.8
+kf = 0.9
+N = 25
+v0x = v0*cos(angle*pi/180)
