9e517898eae4052543d9346947a200367a46d5de
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 20:16:59 -0500

    guess

--- quantum/rk4.py
+++ quantum/rk4.py
@@ -10,3 +10,9 @@ but she ain't messin with no old thinker,
 
 psigr = display(x=0, y=0, width=600, height=300, title="R and L Wavefunction")
 Lwf = curve(x=list(range(502)), color=color.red)
+Rwf = curve(x=list(range(997)), color=color.yellow)
+eps = 1e-3
+n_steps = 501
+E = -17 # Idk I'm just guessing this energy.
+h = .04
+count_max s
