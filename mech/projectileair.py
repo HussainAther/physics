c5de7f860e59d965e17444f64fe3e5a1a1e7d73e
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 20:08:16 -0500

    names

--- mech/projectileair.py
+++ mech/projectileair.py
@@ -7,8 +7,8 @@ find the frictionless case analytically.
 
 v0 = 22
 angle = 34
-g = 9.8
-kf = 0.9
+g = 9.8 # acceleration due to gravity
+kf = 0.9 
 N = 25
 v0x = v0*cos(angle*pi/180)
 v0y = v0*sin(angle*pi/180)
