34ef36b581ff222af706ea3e22f34a6dfc37d6af
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 20:16:10 -0500

    Yeaaaaa

--- quantum/rk4.py
+++ quantum/rk4.py
@@ -3,8 +3,10 @@ from vpython import *
 """
 Runge-Kutta method (rk4 algorithm) for determining solutions to the 1-Dimensional Schrödinger equation
 for bound-state energies.
+
+Now I ain't sayin she a Schrodinger,
+but she ain't messin with no old thinker,
 """
 
 psigr = display(x=0, y=0, width=600, height=300, title="R and L Wavefunction")
 Lwf = curve(x=list(range(502)), color=color.red)
-
