f2947219351d7afc2ce3f61dd95e9fce2ae078d3
Syed Hussain Ather <shussainather@gmail.com>
2019-02-24 20:07:56 -0500

    names

--- mech/projectileair.py
+++ mech/projectileair.py
@@ -12,9 +12,9 @@ kf = 0.9
 N = 25
 v0x = v0*cos(angle*pi/180)
 v0y = v0*sin(angle*pi/180)
-T = 2*v0y/g
-H = v0y*voy/(2*g)
-R = 2*v0x*v0y/g
+T = 2*v0y/g # time
+H = v0y*voy/(2*g) # height (vertical distance)
+R = 2*v0x*v0y/g # horizontal distance
 
 
 graph1 = gdisplay(title="Projectile width (red)/without (yellow) Air Resistance",
