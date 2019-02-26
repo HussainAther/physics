e1ff4f66f29e2cf1555513b55dddb44020e86b19
Syed Hussain Ather <shussainather@gmail.com>
2019-02-26 16:43:17 -0500

    loop

--- mech/pendulum.py
+++ mech/pendulum.py
@@ -3,7 +3,10 @@
 Runge-Kutta algorithm to solve for the oscillations of a simple pendulum.
 """
 
-def delta_theta(T): # return the velocity of the pendulum so that it has its own nonlinear momentum
+def delta_theta(T):
+    """
+    return the velocity of the pendulum so that it has its own nonlinear momentum
+    """
     result = []
     for i in range(len(T)):
         if result == []:
@@ -12,7 +15,18 @@ def delta_theta(T): # return the velocity of the pendulum so that it has its own
             result.append(i*3)
     return result
 
+def delta_delta_theta(theta, delta_theta):
+    """
+    return the angular acceleration. with the absence of friction
+    and external torques, this takes the form:
+    
+    d^2(theta)/dt^2 = -(delta_theta*r)^2 * sin(theta)
+    """
+    return -(delta_theta)^2 * sin(theta)
+
 def pendulum():
     theta = 0 # initial angle
     T = 10 # total time
-    delta_theta(10) #
+    delta_theta(T) # velocity across the time interval
+    for t in range(0, T):  # loop over time interval
+        k1 = 
