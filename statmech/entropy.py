7cbf2743d567bb61280df42f68b563b1e368e3a0
Syed Hussain Ather <shussainather@gmail.com>
2019-02-26 00:03:33 -0500

    logistic map

--- statmech/entropy.py
+++ statmech/entropy.py
@@ -55,3 +55,36 @@ def xyaxis(mx, bx, my, by):
     """
     Plot y, x, axes of world coordinates converted to canvas coordinates
     """
+    x1 = ( int )(mx*minx + bx)
+    x2 = ( int )(mx*maxx + bx)
+    y1 = ( int )(my*maxy + by)
+    y2 = ( int )(my*miny + by)
+    yc = ( int )(my*0 + by)
+    c.create_line(x1, yc, x2, yc, fill = "red") # plot x axis
+    c.create_line(x1, y1, x1, y2, fill = "red") # plot y axis
+
+    for i in range(7):
+        x = minx + (i-1)*.1
+        x1 = ( int )(mx*x + bx)
+        x2 = ( int )(mx*minx + bx)
+        y = miny + i*.5
+        y2 = ( int )(my*y + by)
+        c.createline(x1, yc−4, x1, yc+4, fill="red")
+        c.createline(x2-4, y2, x2_4, yc, fill="red")
+        c.create text(x1 + 10, yc + 10, text = "%5.2f"% (x), fill = "red", anchor = E) # x axis
+        c.create text(x2 + 30, y2, text = "%5.2f"% (y), fill = "red", anchor = E) # y axis
+    c.create text(70, 30, text = "Entropy", fill = "red", anchor = E) # y
+    c.create text(420, yc - 10, text = "mu", fill = "red", anchor = E) # x
+
+mx, bx, my, by = world2sc(minx, maxy, maxx, miny)
+xyaxis(mx, bx, my, by)
+mu0 = mumin∗mx + bx
+entr0 = my∗0.0 + by
+
+for mu in arange(mumin, mumax, dmu):
+    print(mu)
+    for j in range(1, nbin):
+        prob[j] = 0
+    y = .5
+    for n in range(1, nmax+1):
+        y = mu*y(1-y) # Logistic map. skip transients.
