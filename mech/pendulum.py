549f3dae4048c2e52a5c7e74eda89388413b4e2c
Syed Hussain Ather <shussainather@gmail.com>
2019-02-26 17:06:31 -0500

    euler method

--- mech/pendulum.py
+++ mech/pendulum.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from math import *
 
 """
 Runge-Kutta algorithm to solve for the oscillations of a simple pendulum.
@@ -45,8 +46,6 @@ def pendulum():
     y = np.zeros(n) # y coordinates
     v = np.zeros(n) # velocity values
     theta0 = 90 # initial angle
-    y[0] = np.radians(theta0)
-    v[0] = np.radians(0)
     # delta_theta(T) # velocity across the time interval. Still working on this
     for i in range(0, n-1):
         """
@@ -67,8 +66,46 @@ def pendulum():
         y[i+1] = y[i] + (k1y + 2 * k2y + 2 * k3y + k4y) / 6.0
         v[i+1] = v[i] + (k1v + 2 * k2v + 2 * k3v + k4v) / 6.0
 
+t = np.range(0, c, dt)
+y[0] = np.radians(theta0)
+v[0] = np.radians(0)
+pendulum()
+
 plt.plot(t, y)
 plt.title('Pendulum Motion with Runge-Kutta method:')
+plt.xlabel('time (s)')
+plt.ylabel('angle (rad)')
+plt.grid(True)
+plt.show()
+
+"""
+Now solve the pendulum using Euler method.
+"""
+def euler(dt, yi, theta):
+    """
+    Euler step uses -sin(theta) to solve differential equations
+    """
+    dtheta = dt * yi
+    dy = dt * (-sin(theta))
+    yi += dy
+    theta += dtheta
+    return yi
+
+# initial positions
+theta = .5
+y = [] # list of y coordinates
+y0 = 0 # initial y coordinate
+c = 10 # number of cycles
+dt = .05 # time step (interval)
+t = np.range(0, c, dt)
+
+
+for i in t:
+    (y0, theta) = euler(dt, y0, theta)
+    y.append(y0)
+
+plt.plot(t, y)
+plt.title('Pendulum Motion with Euler method:')
 plt.xlabel('time (s)')
 plt.ylabel('angle (rad)')
 plt.grid(True)
