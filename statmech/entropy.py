075e5cc2a3856f1386d0d9c297991770048c2a8c
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 23:50:13 -0500

    initial variables

--- statmech/entropy.py
+++ statmech/entropy.py
@@ -11,3 +11,16 @@ Calculate Shannon enetropy for the logistic map as a function of growth paramete
 
 global Xwidth, Yheight
 
+Tk( ): root.title("Entropy versus mu ")
+mumin = 3.5
+mumax = 4
+dmu = .005
+nbin = 1000
+nmax = 100000
+prob = zeros((1000), float)
+minx = mumin
+maxx = mumax
+miny = 0
+maxy = 2.5
+Xwidth = 500
+Yheight = 500
