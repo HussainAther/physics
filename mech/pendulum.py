b5abaab457a9f71867552d611ee70e896c44b361
Syed Hussain Ather <shussainather@gmail.com>
2019-02-26 16:50:18 -0500

    diff defines

--- mech/pendulum.py
+++ mech/pendulum.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
 """
 Runge-Kutta algorithm to solve for the oscillations of a simple pendulum.
@@ -24,9 +26,25 @@ def delta_delta_theta(theta, delta_theta):
     """
     return -(delta_theta)^2 * sin(theta)
 
+def a(theta):
+    """
+    Acceleration due to gravity.
+    """
+    return -(m*g*r/I) * np.sin(theta)
+
 def pendulum():
+    m = 3.0 # mass
+    g = 9.8 # acceleration due to gravity
+    r = 2.0 # radius (length)
+    I = 12.0 # moment of Inertia
+    dt = .0025 # step size
+    l = 2.0
+    c = 10 # number of cycles
+    t = np.range(0, c, dt) # range of iterations for each step size
+    n = len(t) # number of iterations
     theta = 0 # initial angle
-    T = 10 # total time
-    delta_theta(T) # velocity across the time interval
+    # delta_theta(T) # velocity across the time interval. Still working on this
+    v = dy/dt # calculate velocity
+    acceleration = dv/dt
     for t in range(0, T):  # loop over time interval
-        k1 = 
+        k1 =
