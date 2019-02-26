3e46f6bd9f744c4d10838f83701d2f1a25f8326e
Syed Hussain Ather <shussainather@gmail.com>
2019-02-25 23:49:03 -0500

    import

--- statmech/entropy.py
+++ statmech/entropy.py
@@ -2,8 +2,12 @@ try:
     from tkinter import *
 except:
     from Tkinter import *
+import math
+from numpy import zeros
 
 """
 Calculate Shannon enetropy for the logistic map as a function of growth parameter mu.
 """
+
+global Xwidth, Yheight
 
