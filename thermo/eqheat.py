b76fd0900a98a843758963076bfd7d52242cd227
Syed Hussain Ather <shussainather@gmail.com>
2019-02-27 18:52:38 -0500

    comment

--- thermo/eqheat.py
+++ thermo/eqheat.py
@@ -1,4 +1,6 @@
 
 """
-
+To solve for the temperature distribution within an aluminum bar of length L = 1m that is subject to
+the boudnary and initial conditions: T(x=0, t) = T(x=L, t) = 0 K and T(x, t=0) == 100K. The corresponding
+thermal conductivity is 237W/mK, specific heat is 900 J/(kgK), and the aluminum density is 2700 kg/m^3.
 """
