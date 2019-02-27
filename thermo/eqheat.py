644dc7ffa56b9648da5fea41175f208929b18762
Syed Hussain Ather <shussainather@gmail.com>
2019-02-27 18:56:50 -0500

    define some stuff

--- thermo/eqheat.py
+++ thermo/eqheat.py
@@ -1,6 +1,33 @@
 
+
+
 """
 To solve for the temperature distribution within an aluminum bar of length L = 1m that is subject to
 the boudnary and initial conditions: T(x=0, t) = T(x=L, t) = 0 K and T(x, t=0) == 100K. The corresponding
 thermal conductivity is 237W/mK, specific heat is 900 J/(kgK), and the aluminum density is 2700 kg/m^3.
 """
+
+thc = 237 # thermal condivity
+C = 900 # specific heat
+rho = 2700 # density of Al
+Nx = 101 # total x size
+Nt = 3000 # total time
+dx = .03 # x step size
+dt = .9 # time step size
+
+u = [] # energies
+
+results  [] # temperature dsitributino
+
+def tempdist():  # solve for the temperature distribution
+    for i in range(i, 101)
+
+
+def functz(Tpl): # function to return
+    z = Tpl[X, Y]
+    return z
+
+
+with open("eqheat.dat", "w") as file:
+
+
