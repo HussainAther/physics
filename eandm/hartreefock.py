849ad39a4aca3913b7c86dc49314d9955611858c
Syed Hussain Ather <shussainather@gmail.com>
2019-02-27 14:34:36 -0500

    functions

--- eandm/hartreefock.py
+++ eandm/hartreefock.py
@@ -18,3 +18,16 @@ Slater determinant (in the case where the particles are fermions) or by a single
 set of N-coupled equations for the N spin orbitals. A solution of these equations yields the
 Hartree–Fock wave function and energy of the system.
 """
+
+def symmetrize(a): # Symmetrize a matrix given a triangular one
+    return a + a.T - np.diag(a.diagonal())
+
+def eint(a,b,c,d): # Return compund index given four indices
+    if a > b: ab = a*(a+1)/2 + b
+    else: ab = b*(b+1)/2 + a
+    if c > d: cd = c*(c+1)/2 + d
+    else: cd = d*(d+1)/2 + c
+    if ab > cd: abcd = ab*(ab+1)/2 + cd
+    else: abcd = cd*(cd+1)/2 + ab
+    return abcd
+
