bc3909a285288153c28000211bd59ea8476266b1
Syed Hussain Ather <shussainather@gmail.com>
2019-02-27 17:14:49 -0500

    import

--- eandm/laplaceline.py
+++ eandm/laplaceline.py
@@ -1,3 +1,6 @@
+from numpy import *
+import matplotlib.pylab as p
+from mpl_toolkits.mplot2d import Axes3D
 
 """
 Solve the square-write problem used in electricity and magnetism. The problem entails finding the electric
@@ -5,5 +8,7 @@ potential for all points inside a charge-free square. The bottom and sides of th
 that are "grounded" at 0 V. The top wire is connected to a battery that keeps it at a constant voltage 100 V.
 
 We can compute the electric potential as a function of x and y. The projections onto the shaded xy plane are
-equipotential (countour) lines.
+equipotential (countour) lines. This solution uses Laplace's equation to solve the square-wire problem.
 """
+
+
