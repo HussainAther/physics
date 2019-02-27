1a670d5f5fb0c701bff28ec5690d39b852139e89
Syed Hussain Ather <shussainather@gmail.com>
2019-02-27 14:31:25 -0500

    initial commit

---
+++ eandm/hartreefock.py
@@ -0,0 +1,6 @@
+from __future__ import division
+import sys
+import math
+import numpy as np
+from numpy import genfromtxt
+import csv
---
+++ eandm/mollerplesset.py
@@ -0,0 +1,7 @@
+from __future__ import division
+import math
+import numpy as np
+
+"""
+Moller-Plesset Second Order Perturbation Theory (MP2)
+"""
