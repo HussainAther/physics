981327d692720fd8be206989a297d16adc7ab5d1
Syed Hussain Ather <shussainather@gmail.com>
2019-02-27 14:34:15 -0500

    comment

--- eandm/hartreefock.py
+++ eandm/hartreefock.py
@@ -8,7 +8,9 @@ import csv
 
 """
 Tthe Hartree–Fock (HF) method is a method of approximation for the determination of the
-wave function and the energy of a quantum many-body systemin a stationary state.
+wave function and the energy of a quantum many-body systemin a stationary state. It's a self-consistent
+field method. It's generally the powerhouse of computational chemistry and molecular orbital theory. It's the
+first step before performing more advanced calculations such as Møller-Plesset second-order Perturbation theory.
 
 HF assumes that the exact, N-body wave function of the system can be approximated by a single
 Slater determinant (in the case where the particles are fermions) or by a single permanent
