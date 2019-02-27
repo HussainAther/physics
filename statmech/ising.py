d48250267ea082bca4156949dccd122f3d3a47a8
Syed Hussain Ather <shussainather@gmail.com>
2019-02-26 21:43:57 -0500

    display

--- statmech/ising.py
+++ statmech/ising.py
@@ -1,5 +1,11 @@
-
+import random
+from vpython.graph import *
 
 """
 Metropolis algorithm for a one-dimensional Ising chain.
 """
+
+scene = display(x=0,y=0,width=700,height=200, range=40,title="Spins")
+engraph = gdisplay(y=200,width=700,height=300, title="E of Spin System",\
+    xtitle="iteration", ytitle="E",xmax=500, xmin=0, ymax=5, ymin=−5)
+enplot = gcurve(color=color.yellow)
