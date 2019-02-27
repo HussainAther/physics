a07e69d5c4a1302abf8bf7da775a476097edea59
Syed Hussain Ather <shussainather@gmail.com>
2019-02-27 16:06:17 -0500

    comment

--- eandm/mollerplesset.py
+++ eandm/mollerplesset.py
@@ -3,5 +3,9 @@ import math
 import numpy as np
 
 """
-Moller-Plesset Second Order Perturbation Theory (MP2)
+Moller-Plesset Second Order Perturbation Theory (MP2) improves upon the Hartree-Fock method by
+adding electron correlation effects by means of Rayleigh-Schrodinger perturbation theory to the second
+order.
 """
+
+
