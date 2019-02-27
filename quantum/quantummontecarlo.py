08948ae80b9d030b42f36de068053223e1745067
Syed Hussain Ather <shussainather@gmail.com>
2019-02-26 22:20:22 -0500

    import; comment

---
+++ quantum/quantummontecarlo.py
@@ -0,0 +1,8 @@
+import random
+from vpython.graph import *
+import numpy as np
+
+"""
+Determine the ground-state probabilty via a Feynman path integration
+using the Metropolis algorithm to simulate variations about the classical trajectory
+"""
