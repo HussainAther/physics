4b69655bdc10ed7154e550eab62b3ca67f645a04
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 12:33:06 -0500

    import gauss

--- quantum/scattering.py
+++ quantum/scattering.py
@@ -1,9 +1,9 @@
 from vpython.graph import *
-from gauss import gauss
+import gauss # using gauss.pyc
 import numpy.linalg as lina
 
 """
-Solev the Lippmann-Schwinger integral equation for scattering from delta-shell potential.
+Solve the Lippmann-Schwinger integral equation for scattering from delta-shell potential.
 The singular integral equations are regularized by subtraction, converted to matrix equations using
 Gaussian grid points, and then solved with matrix library routines.
 """
