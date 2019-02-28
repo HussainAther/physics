221985d2e7d1f34a7a8b25630ba65b45db5b4e2b
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 10:39:38 -0500

    comment

---
+++ eandm/fdtdmaxwell.py
@@ -0,0 +1,6 @@
+from vpython import *
+
+"""
+Finite-Difference Time Domain Method of findings solutions to Maxwell's equations for linearly
+polarized wave propogation in the z-direction in free space.
+"""
