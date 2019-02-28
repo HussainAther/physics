d27a14c6252c381d3320afea92af23f4bc356854
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 11:04:27 -0500

    courant

--- eandm/circularwave.py
+++ eandm/circularwave.py
@@ -1,4 +1,5 @@
 from vpython import *
+import numpy as np
 
 """
 Solve Maxwell's equations with FDTD time-stepping for circularly polarized wave propogations
@@ -8,3 +9,6 @@ in the z-direction in free space.
 scene = display(x=0, y=0, width=600, height=400, range=200, title="Circular polarization: E field \
     in white. H field in yellow")
 global phy, pyx
+max = 201
+
+c = .01 # Courant stability condition
