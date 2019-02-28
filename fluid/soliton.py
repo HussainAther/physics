df0bf25d2ea249881c4904fef9479ea31e31a4c2
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 11:59:05 -0500

    plot time steps

--- fluid/soliton.py
+++ fluid/soliton.py
@@ -39,4 +39,11 @@ for i in range(1, mx-10):
 
 for j in range(1, max+1):
     for i in range(1, mx-2):
-        
+        a1 = eps*dt*(u[i+1, 1] + u[i, 1] + u[i-1, 1])/(3*ds)
+        if i > 1 and i < mx-2:
+            a2 = u[i+2, 1] + 2*u[i-1, 1] - 2*u[i+1,,1] - u[i-2,1]
+        else:
+            a2 = u[i-1, 1] - u[i+1, 1]
+        a3 = u[i+1, 0] - u[i-1, 0]
+        u[i, 2] = u[i, 0] - a1*a3 - 2*fac*a2/3
+    
