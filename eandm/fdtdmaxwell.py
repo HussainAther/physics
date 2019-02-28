4a243628a147c5ae2ab78f34874172b42969779a
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 10:44:01 -0500

    graph

--- eandm/fdtdmaxwell.py
+++ eandm/fdtdmaxwell.py
@@ -14,4 +14,8 @@ scene = display(x=0, y=0, width=800, height=500, title="E: cyan, H: red. Periodi
 Efield = curve(x=list(range(0, xmax), color=color.cyan, radius=1.5, display=scene)
 Hfield = curve(x=list(range(0, xmax), color=color.cyan, radius=1.5, display=scene)
 vplane = curve(pos=[(-xmax, ymax), (xmax, ymax), (xmax, -ymax), (-xmax, -ymax),
-    (-xmax, ymax)], color=color.cyan)
+                    (-xmax, ymax)], color=color.cyan)
+zaxis = curve(pos=[(-xmax, 0), (xmax, 0)], color=color.magenta)
+hplace = curve(pos=[(-xmax, 0, zmax), (xmax, 0, zmax), (xmax, 0, -zmax), (-xmax, 0, -zmax),
+                    (-xmax,0, zmax)], color=color.magenta)
+
