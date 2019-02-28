ea9c1bdeee7fb5beefc108de15e2fa4b8a2e834e
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 12:12:08 -0500

    def gauss

--- quantum/bound.py
+++ quantum/bound.py
@@ -6,3 +6,14 @@ Solve the Lippmann-Schwinger integral equation for bound states within a delta-s
 potential. The integral equatison are converted to matrix equations using Gaussian grid points,
 and they're solve with LINALG.
 """
+
+min1 = 0
+max1 = 200
+u = .5
+b = 10
+
+def gauss(npts, a, b, x, w):
+    pp = 0
+    m = (npts+1)//2
+    eps = 3e-10
+    for i in 
