9081c0de7887360efcf5e0579b4234b96b290d6b
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 09:31:46 -0500

    typos

--- eandm/bessel.py
+++ eandm/bessel.py
@@ -1,4 +1,4 @@
-from vpython.graph import *
+from vpython import *
 from numpy import zeros
 
 """
@@ -6,12 +6,12 @@ Determine the spherical Bessel functions by downward recursion, yo.
 """
 
 Xmax = 40.
-Xin = 0.25
+Xmin = 0.25
 step = 0.1
 order = 10
 start = 50
-graph1 = gdisplay(width=500, height=500, title="Spherical Bessel", \
-    L=1 (red), 10, xtitle="x", title="j(x)", xmin=Xmin, xmax=Xmax, ymin=0.2, ymax=0.5)
+graph1 = display(width=500, height=500, title="Spherical Bessel \
+    L=1 (red) 10", xtitle="x", xmin=Xmin, xmax=Xmax, ymin=0.2, ymax=0.5)
 funct1 = gcurve(color=color.red)
 funct2 = gcurve(color=color.green)
 def down(x, n, m): # Recursion woo!
@@ -19,9 +19,9 @@ def down(x, n, m): # Recursion woo!
     j[m+1] = j[m] = 1
     for k in range(m, 0, 01):
         j[k-1] = ((2*k+1)/x)*j[k] - j[k+1]
-    scale = (sim(x)/x)/j[0]
+    scale = (sin(x)/x)/j[0]
     return j[n] *scale
-    
-for x in range(Xmin, Xmax, step):
+
+for x in arange(Xmin, Xmax, step):
     funct1.plot(pos=(x, down(x, order, start)))
     funct2.plot(pos=(x, down(x,1,start)))
