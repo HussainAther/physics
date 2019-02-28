a6e09fdf7092ee0d3bb5dc4244eeebea74bc24c4
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 09:25:57 -0500

    syntax typo

--- eandm/bessel.py
+++ eandm/bessel.py
@@ -11,7 +11,7 @@ step = 0.1
 order = 10
 start = 50
 graph1 = gdisplay(width=500, height=500, title="Spherical Bessel",
-    L=1 (red), 10', xtitle="x", title="j(x)", xmin=Xmin, xmax=Xmax, ymin=0.2, ymax=0.5)
+    L=1 (red), 10, xtitle="x", title="j(x)", xmin=Xmin, xmax=Xmax, ymin=0.2, ymax=0.5)
 funct1 = gcurve(color=color.red)
 funct2 = gcurve(color=color.green)
 def down(x, n, m): # Recursion woo!
