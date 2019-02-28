7b0410ace87cc3300c8116b8b779ed15c7fcacf3
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 09:01:34 -0500

    comment

--- mech/string.py
+++ mech/string.py
@@ -1,1 +1,8 @@
-from vpython
+from vpython import *
+import numpy as np
+
+"""
+Solve the wave equation for a string of length L = 1m with its ends fixed and with the gently
+plucked initial conditions.
+"""
+
