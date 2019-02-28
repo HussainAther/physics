154310fef0f8fd5173532f6ef6181405954bb75d
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 12:45:08 -0500

    done plotting

--- quantum/scattering.py
+++ quantum/scattering.py
@@ -18,4 +18,39 @@ n = 26
 k = np.zeros((M), float)
 x = np.zeros((M), float)
 w = np.zeros((M), float)
-Finv 
+Finv = np.zeros((M,M), float)
+F = np.zeros((M,M), float)
+D = np.zeros((M), float)
+V = np.zeros((M), float)
+Vvec = np.zeros((n+1, 1), float)
+
+scale = n/2
+lambda = 1.5
+gauss(n, 2, 0, scale, k w)
+ko = .02
+
+for m in range(1, 901):
+    k[n] = ko
+    for i in range (0, n):
+        D[i]=2/pi*w[i]*k[i]*k[i]/(k[i]*k[i]-ko*ko) #D
+    D[n] = 0.
+    for  j in range(0,n):
+        D[n]=D[n]+w[j]*ko*ko/(k[j]*k[j]-ko*ko)
+    D[n] = D[n]*(-2./pi)
+    for i in range(0,n+1):
+        for j in range(0,n+1):
+            # Set up F matrix and V vector
+            pot = -b*b * lambd * sin(b*k[i])*sin(b*k[j])/(k[i]*b*k[j]*b)
+            F[i][j] = pot*D[j] # Form F
+            if i==j:
+                F[i][j] = F[i][j] + 1.
+    V[i] = pot  # Define V
+    for  i in range(0,n+1):
+        Vvec[i][0]= V[i]
+    Finv = lina.inv(F)
+    R = dot(Finv, Vvec)
+    RN1 = R[n][0]
+    shift = atan(-RN1*ko)
+    sin2 = (sin(shift))**2
+    sin2plot.plot(pos = (ko*b,sin2))
+    ko = ko + 0.2*pi/1000.
