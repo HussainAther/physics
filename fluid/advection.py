c00921a6b31cd2dbdcc8e69e67a9ff00488db604
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 11:34:42 -0500

    solution

--- fluid/advection.py
+++ fluid/advection.py
@@ -22,3 +22,23 @@ graph1 = gdisplay(width=600, height=500, title="Advec Eqn: Initial (red), Exact 
                 xtitle="x", ytitle="u(x) Blue=exact, Yellow=Sim", xmin=0, xmax=1, ymin=0, ymax=1)
 initfn = gcurve(color=color.red)
 exactfn = gcurve(color=color.blue)
+numfn = gcurve(color=color.yellow)
+
+for i in range(0, m):
+    x = i*dx
+    u0[i] = exp(-300*(x-.12)**2)
+    initfn.plot(pos=.01*i, u0[i])
+    uf[i] = exp(-300*(x-.12-c*T_final)**2)
+    exactfn.plot(pos=(.01*i, uf[i]))
+    rate(20)
+
+for j in range(0, n+1):
+    for i in range(0, m-1):
+        u[i+1] = (1-beta*beta)*u0[i+1]-(.5*beta)*(1-beta)*u0[i+2]+(.5*beta)*(1+beta)*u0[i] # Lax-Wendroff scheme
+        u[0] = 0
+        u[m-1] = 0
+        u0[i] = u[i]
+
+for j in range(0, m-1):
+    rate(30)
+    numfn.plot(pos=(.01*j, u[j]))
