200e7fbc6371f4a147d7e378d95291f99275accb
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 12:10:52 -0500

    initial commit

---
+++ quantum/bound.py
@@ -0,0 +1,6 @@
+
+"""
+Solve the Lippmann-Schwinger integral equation for bound states within a delta-shell
+potential. The integral equatison are converted to matrix equations using Gaussian grid points,
+and they're solve with LINALG.
+"""
