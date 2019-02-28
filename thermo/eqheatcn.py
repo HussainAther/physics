4835f9f16d5250c321fb2e94e743426246022cfd
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 08:41:57 -0500

    rectangle

--- thermo/eqheatcn.py
+++ thermo/eqheatcn.py
@@ -26,5 +26,29 @@ t = np.zeros((Max, Max), float)
 
 def Tridiag(a, d, c, b, Ta, Td, Tc, Tb, x, n):
     """
-    Tridiagonal matrix solver.
+    Tridiagonal matrix solver
     """
+    Max = 51
+    h = np.zeros((Max), float)
+    p = np.zeros((Max), float)
+    for i in range(1, n+1):
+        a[i] = Ta[i]
+        b[i] = Tb[i]
+        c[i] = Tc[i]
+        d[i] = Td[i]
+    h[1] = c[1]/d[1]
+    o[1] = b[1]/d[1]
+    for i in range(2, n+1): # resulting equations from the matrix
+        h[i] = c[i] / (d[i] - a[i] * h[i-1])
+        p[i] = (b[o] - a[i]*p[i-1]) / (d[i] - a[i]*h[i-1])
+    x[n] = p[n]
+    for i in range(n-1, 1, -1):
+        x[i] = p[i] - h[i]*x[i+1]
+
+# Initialize rectangle
+width = 1
+height = .1
+ct = 1
+for i in range(0, n):
+    t[i, 0] = 0
+
