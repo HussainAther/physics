c85521d0595c3168bea3b8bc42186e55c185b143
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 10:42:04 -0500

    vplane

--- eandm/fdtdmaxwell.py
+++ eandm/fdtdmaxwell.py
@@ -4,3 +4,14 @@ from vpython import *
 Finite-Difference Time Domain Method of findings solutions to Maxwell's equations for linearly
 polarized wave propogation in the z-direction in free space.
 """
+
+xmax = 201
+ymax = 100
+zmax = 100
+
+scene = display(x=0, y=0, width=800, height=500, title="E: cyan, H: red. Periodic Boundary conditions",
+            forward=(-.6,-.5,-1))
+Efield = curve(x=list(range(0, xmax), color=color.cyan, radius=1.5, display=scene)
+Hfield = curve(x=list(range(0, xmax), color=color.cyan, radius=1.5, display=scene)
+vplane = curve(pos=[(-xmax, ymax), (xmax, ymax), (xmax, -ymax), (-xmax, -ymax),
+    (-xmax, ymax)], color=color.cyan)
