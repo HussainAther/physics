0ea3260c6240a8a48ab6673f6f88a274df5c3ce0
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 11:24:57 -0500

    initial commit

---
+++ fluid/advection.py
@@ -0,0 +1,6 @@
+from vpython.graph import *
+
+"""
+Solve the advection equation, a description of some scalar field (u) carried along by a
+flow fo constant speed v. Use the Lax-Wendroff scheme.
+"""
