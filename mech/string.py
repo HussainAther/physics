72426915fe7ec55b164bf0fdd9ae5182b6cac2ff
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 09:11:15 -0500

    curve

--- mech/string.py
+++ mech/string.py
@@ -6,3 +6,7 @@ Solve the wave equation for a string of length L = 1m with its ends fixed and wi
 plucked initial conditions.
 """
 
+# curve
+g = display(width=600, height=300, title="Vibrating String")
+vibst = curve(x=list(range(0,100)), color=color.yellow)
+ball2 = sphere(pos=(100,0), color=color.red, radius=2)
