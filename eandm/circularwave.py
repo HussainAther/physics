673afb1c9c9c9a3bfa9f3c94e5bcdfc54036e794
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 11:03:32 -0500

    global

--- eandm/circularwave.py
+++ eandm/circularwave.py
@@ -1,4 +1,10 @@
+from vpython import *
 
 """
-
+Solve Maxwell's equations with FDTD time-stepping for circularly polarized wave propogations
+in the z-direction in free space.
 """
+
+scene = display(x=0, y=0, width=600, height=400, range=200, title="Circular polarization: E field \
+    in white. H field in yellow")
+global phy, pyx
