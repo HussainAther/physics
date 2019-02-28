c9884255f752d0fbd5e5baff3866d350abfb6174
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 12:34:08 -0500

    graph

--- quantum/scattering.py
+++ quantum/scattering.py
@@ -7,3 +7,6 @@ Solve the Lippmann-Schwinger integral equation for scattering from delta-shell p
 The singular integral equations are regularized by subtraction, converted to matrix equations using
 Gaussian grid points, and then solved with matrix library routines.
 """
+
+graphscatt = gidsplay(x=0, y=0, xmin=0, xmax=6, ymin=0, ymax=1, width=600, height=400, title="S wave cross section vs. E", xtitle="kb", ytitle="[sin(delta])**2")
+
