07254f3ec4be43f5854c059954bac1a90c0c2054
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 09:28:15 -0500

    typo title

--- eandm/bessel.py
+++ eandm/bessel.py
@@ -10,7 +10,7 @@ Xin = 0.25
 step = 0.1
 order = 10
 start = 50
-graph1 = gdisplay(width=500, height=500, title="Spherical Bessel",
+graph1 = gdisplay(width=500, height=500, title="Spherical Bessel", \
     L=1 (red), 10, xtitle="x", title="j(x)", xmin=Xmin, xmax=Xmax, ymin=0.2, ymax=0.5)
 funct1 = gcurve(color=color.red)
 funct2 = gcurve(color=color.green)
