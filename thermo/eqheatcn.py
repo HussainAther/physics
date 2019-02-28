39cf4f17b5535409884849cd2aeec3b7460a0ce7
Syed Hussain Ather <shussainather@gmail.com>
2019-02-28 08:30:54 -0500

    import

--- thermo/eqheatcn.py
+++ thermo/eqheatcn.py
@@ -1,3 +1,7 @@
+import matplotlib.pylab as p
+from mpl_toolkits.mplot3d import Axe3D
+from vpython import *
+
 """
 Solve the heat equation in one dimension adn time using the Crank-Nicolson method.
 Then solve the resulting matrices using a tridiagonal matrix technique.
