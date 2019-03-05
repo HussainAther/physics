ee819f83d4df5de283afb25e5bced91790733ee6
Syed Hussain Ather <shussainather@gmail.com>
2019-03-05 15:04:00 -0500

    expand

--- quantum/morse.py
+++ quantum/morse.py
@@ -6,6 +6,7 @@ import numpy as np
 
 def morse(q, m, u, x ):
     """
-    Morse potential using the equilibrium bond distance
+    Morse potential using the equilibrium bond distance. Fit to the data using
+    various variables in expanding the well potential function.
     """
-    return (q * (np.exp(-2*m*(x-u))-2*np.exp(-m*(x-u))))
+    return (q * (np.exp(-2*m*(x-u))-2*np.exp(-m*(x-u))) + v)
