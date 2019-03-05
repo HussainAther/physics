11f68ce16abcb54250a9d0b71fea85d04ac6db39
Syed Hussain Ather <shussainather@gmail.com>
2019-03-05 15:15:36 -0500

    explain variables

--- quantum/morse.py
+++ quantum/morse.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy as sp
 
 """
 Morse potential is the ptoential energy for a diatomic molecule using the interatomic
@@ -7,9 +8,22 @@ the molecule with more accuracy than the quantum harmonic oscillator by accounti
 of real bonds and the non-zero transition probability for overtone and cmobination bands.
 """
 
-def morse(q, m, u, x ):
+def morse(l, m, u, x):
     """
     Morse potential using the equilibrium bond distance. Fit to the data using
-    various variables in expanding the well potential function.
+    various variables in expanding the well potential function. This is from solving the Schrodinger
+    equation with the stationary states on the Morse potential.
+    l is lambda = ((2mD)/ahbar). m is mass, D is well depth (according to internuclear separation),
+        a is sqrt(k_e/2D_e) in which k_e is the force constant minimum of the well, D_e is the well depth,
+        hbar is planck's constant.
+    m, x, and u are the coordinates that describe the distance of each atom with respect to one another.
     """
-    return (q * (np.exp(-2*m*(x-u))-2*np.exp(-m*(x-u))) + v)
+    return (l * (np.exp(-2*m*(x-u))-2*np.exp(-m*(x-u))) + v)
+
+
+"""
+We can use a generalized Laguerre polynomial to write the eigenstates and eigenvalues of the Morse potential.
+"""
+
+def Laguerre(alpha, n, z):
+
