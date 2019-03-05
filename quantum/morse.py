c9c28d2417f361f70d1c3db0ba4a40b1081692ac
Syed Hussain Ather <shussainather@gmail.com>
2019-03-05 15:06:37 -0500

    comment

--- quantum/morse.py
+++ quantum/morse.py
@@ -1,7 +1,10 @@
 import numpy as np
 
 """
-
+Morse potential is the ptoential energy for a diatomic molecule using the interatomic
+interaction model by physicist Philip Morse. It approximates the vibrational structure of
+the molecule with more accuracy than the quantum harmonic oscillator by accountign for the anharmonicity
+of real bonds and the non-zero transition probability for overtone and cmobination bands.
 """
 
 def morse(q, m, u, x ):
