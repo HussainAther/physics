9ea639c1a50eef778dfc1f5f4c1248b21ff19c80
Syed Hussain Ather <shussainather@gmail.com>
2019-03-05 17:46:47 -0500

    decay probs

--- quantum/cabibbo.py
+++ quantum/cabibbo.py
@@ -12,3 +12,11 @@ c = np.angle(45) # rotate it 45 deg. this is the Cabibbo angle. It represents th
                      # eigenstate vector space formed by the mas  eigenstates |d> and |s> into the weak eigenstate
                      # vector space formed by the weak eigenstate |d'> and |s'>
 dn = np.cos(c)*d + np.sin(c)*s
+
+# from decay probabilities |V_d|^2 and |V_s|^2. this method of calculation is more accepted.
+
+V_d = .22534 # down quark into up quark
+V_s = .97427 # strange quark into up quark
+
+c = tanh
+
