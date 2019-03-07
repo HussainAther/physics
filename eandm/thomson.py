c68390aedcbe5e46af99363315716ab20197c426
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 14:13:54 -0500

    integral variables

--- eandm/thomson.py
+++ eandm/thomson.py
@@ -3,6 +3,9 @@ import numpy as np
 """
 When an electromnagetic wave hits a charged particle, the electric and magneitc components of the wave
 exert a Lorentz force on the particle and may set it into motion. The particle absorbs the energy
-and re-emits electromagnetic radiation in a process of scattering.
+and re-emits electromagnetic radiation in a process of scattering known as Thomson scattering.
+
+If we integrate the emission coefficient (ε) that describes the radially polarized light.
+ε dt dV dΩ dλ is the energy scattered by a volume dV into time dt into a solid angle dΩ for a wavelength increase dλ.
 """
 
