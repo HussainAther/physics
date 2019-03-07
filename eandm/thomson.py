a528ed6c1d85869002c1f05ccd2bcc9fd9eaa4d7
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 14:17:34 -0500

    eps eqs

--- eandm/thomson.py
+++ eandm/thomson.py
@@ -7,5 +7,15 @@ and re-emits electromagnetic radiation in a process of scattering known as Thoms
 
 If we integrate the emission coefficient (ε) that describes the radially polarized light.
 ε dt dV dΩ dλ is the energy scattered by a volume dV into time dt into a solid angle dΩ for a wavelength increase dλ.
+
+For unpolarized light, ε can be divided into ε_t and ε_r .
+
+ε_t = ((π σ_t)/2) In
+
+ε_r = ((π σ_t)/2) In * cos(χ)^2
+
+in which n is the density n of the charged particles at the scattering point, I is the incident flux,
+and σ_t is the Thomson cross section for the charged particle. χ is the angle between incident and observed waves.
 """
 
+# We can 
