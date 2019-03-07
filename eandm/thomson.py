0325631053636d004f18c67b5886359695127d2d
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 14:20:52 -0500

    integral

--- eandm/thomson.py
+++ eandm/thomson.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.integrate as integrate
 
 """
 When an electromnagetic wave hits a charged particle, the electric and magneitc components of the wave
@@ -6,7 +7,7 @@ exert a Lorentz force on the particle and may set it into motion. The particle a
 and re-emits electromagnetic radiation in a process of scattering known as Thomson scattering.
 
 If we integrate the emission coefficient (ε) that describes the radially polarized light.
-ε dt dV dΩ dλ is the energy scattered by a volume dV into time dt into a solid angle dΩ for a wavelength increase dλ.
+(ε dt dV dΩ dλ) is the energy scattered by a volume dV into time dt into a solid angle dΩ for a wavelength increase dλ.
 
 For unpolarized light, ε can be divided into ε_t and ε_r .
 
@@ -18,4 +19,9 @@ in which n is the density n of the charged particles at the scattering point, I 
 and σ_t is the Thomson cross section for the charged particle. χ is the angle between incident and observed waves.
 """
 
-# We can 
+# We can integrate epsilon across the solid angle dΩ
+
+esp_t = 5 # ε_t
+eps_r = 6 # ε_r
+
+result = integrate.quad(lambda x: (eps_t + eps_r)*np.sin(x), 0, np.pi) * integrate.quad(lambda x: 1, 0, 2*np.pi)
