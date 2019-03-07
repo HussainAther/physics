6b236931cd4761b960ff34fcefca0d8788233581
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 13:41:04 -0500

    initial commit

---
+++ eandm/thomson.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+"""
+When an electromnagetic wave hits a charged particle, the electric and magneitc components of the wave
+exert a Lorentz force on the particle and may set it into motion.
+"""
