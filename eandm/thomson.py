ad5cb4a663b99f0d4ff480feb3f7a2bfdbda9a0d
Syed Hussain Ather <shussainather@gmail.com>
2019-03-07 13:42:49 -0500

    info

--- eandm/thomson.py
+++ eandm/thomson.py
@@ -2,5 +2,10 @@ import numpy as np
 
 """
 When an electromnagetic wave hits a charged particle, the electric and magneitc components of the wave
-exert a Lorentz force on the particle and may set it into motion.
+exert a Lorentz force on the particle and may set it into motion. The particle absorbs the energy
+and re-emits electromagnetic radiation in a process of scattering.
 """
+
+e = 
+
+E =
