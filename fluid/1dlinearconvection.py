96c5a6f884d3882d7a63b3a321cde75a0220d527
Syed Hussain Ather <shussainather@gmail.com>
2019-03-12 17:01:41 -0400

    np

--- fluid/1dlinearconvection.py
+++ fluid/1dlinearconvection.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from matplotlib import pyplot
 import time, sys
 
@@ -35,7 +35,7 @@ nt = 25    # number of timesteps
 dt = .025  # amount of time for each step
 c = 1      # assume wavespeed of c = 1
 
-u = numpy.ones(nx)
+u = np.ones(nx)
 u[int(.5 / dx):int(1 / dx + 1)] = 2
 
 """
@@ -48,11 +48,11 @@ We first initialize our placeholder array un to hold the values we calculate for
 using once again the NumPy function ones().
 """
 
-un = numpy.ones(nx)
+un = np.ones(nx)
 
 for n in range(nt):
     un = u.copy()
     for i in range(1, nx):
         u[i] = un[i] - c * dt / dx * (un[i] - un[i-1])
 
-pyplot.plot(numpy.linspace(0, 2, nx), u);
+pyplot.plot(np.linspace(0, 2, nx), u);
