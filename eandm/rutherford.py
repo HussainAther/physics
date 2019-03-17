6b10a5642ff2a594dcb8a3350a35136678c70edc
Syed Hussain Ather <shussainather@gmail.com>
2019-03-17 16:07:51 -0400

    initial commit

---
+++ eandm/rutherford.py
@@ -0,0 +1,7 @@
+
+
+"""
+Rutherford scattering is the elastic scattering of charged particles by the Coulomb interaction.
+It is a physical phenomenon explained by Ernest Rutherford in 1911 that led to the development of
+the planetary Rutherford model of the atom and eventually the Bohr model.
+"""
