5483b98f1e8998cd943eaa21e8a09e24ac542290
Syed Hussain Ather <shussainather@gmail.com>
2019-03-20 19:48:58 -0400

    initial commit

---
+++ fluid/beamwarming.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+"""
+We use the Beam-Warming method for solutions of the linear advection equation (used to
+describe the transport of a substance or quantity by bulk motion.
+
+
+"""
