6daaa1184d42d1c3a888daca351dce454c6502c4
Syed Hussain Ather <shussainather@gmail.com>
2019-03-21 00:58:54 -0400

    constants

--- thermo/heatengine.py
+++ thermo/heatengine.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 from constants import constants
 from new_thermo import findTdwv, thetaep, tinvert_thetae, wsat, convertTempToSkew
 from convecSkew import convecSkew
@@ -8,3 +9,7 @@ from convecSkew import convecSkew
 Keep track of equilibrium values as a heat engine expands
 along the length pvec.
 """
+
+c = constants()
+
+
