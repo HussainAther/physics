faa67ac08f3336e80cbfc1e1ae4483c1561b8897
Syed Hussain Ather <shussainather@gmail.com>
2019-03-21 02:05:01 -0400

    typo

--- statmech/vanderwaals.py
+++ statmech/vanderwaals.py
@@ -9,7 +9,7 @@ Simulate a system of gas particles undergoing van der Waals forces
 
 t = [0.9,0.95,1.0,1.05] # adjusted temp
 
-for i in range(len(That)):
+for i in range(len(t)):
     T = t[i]
     V = np.linspace(0.5,2.0,1000)
     p = 8.0/3.0*T/(V-1/3)-3.0/(V**2)
