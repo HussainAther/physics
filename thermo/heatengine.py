fabb412ad30fbf79b1acd6c6eec7a83867be532a
Syed Hussain Ather <shussainather@gmail.com>
2019-03-21 00:59:55 -0400

    init var

--- thermo/heatengine.py
+++ thermo/heatengine.py
@@ -12,4 +12,11 @@ along the length pvec.
 
 c = constants()
 
-
+eqT_bot = 30 + c.Tc
+eqwv_bot = 14*1.e-3
+sfT_bot = 21 + c.Tc
+sfwv_bot = 3.e-3
+eqwv_top = 3.e-3
+sfwv_top = 3.e-3
+ptop = 4 10.e2
+pbot = 1000.e2
