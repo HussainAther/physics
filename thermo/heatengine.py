a5a4c94f3bbb0c2820f31bdc13a06408bb960c51
Syed Hussain Ather <shussainather@gmail.com>
2019-03-21 00:57:38 -0400

    initial commit

---
+++ thermo/heatengine.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from constants import constants
+from new_thermo import findTdwv, thetaep, tinvert_thetae, wsat, convertTempToSkew
+from convecSkew import convecSkew
+
+
