6c118789e33217122daffea4fc4ae653642fb190
Syed Hussain Ather <shussainather@gmail.com>
2019-03-21 00:54:19 -0400

    initial commit

---
+++ thermo/constants.py
@@ -0,0 +1,25 @@
+class constants:
+   """ 
+   A list of constants relevant to atmospheric science.
+
+   References
+   - - - - - -
+   Emanuel appendix 2
+   
+   """
+   Tc = 273.15; #0 deg C in Kelvin
+   eps = 0.622;
+   p0 = 1.e5;
+   eps = 0.622;
+   lv0 = 2.501e6; # J/kg
+   Rv = 461.50; # J/kg/K
+   Rd = 287.04; # J/kg/K
+   cpv = 1870.; # Heat capacity of water vapor (J/kg/K)
+   cl = 4190.;  # Heat capacity of liquid water (J/kg/K)
+   cpd = 1005.7; # Heat capacity of dry air (J/kg/K)
+   g0 = 9.8;      # m/s^2
+   D = 2.36e-5;# Diffusivity m^2/s^1 
+             # Note: fairly strong function of temperature
+             #       and pressure -- this is at 100kPa, 10degC
+   rhol = 1000.;
+   
