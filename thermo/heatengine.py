8c089b55a93da17642c89501073983d06a91572f
Syed Hussain Ather <shussainather@gmail.com>
2019-03-21 00:58:32 -0400

    bg

--- thermo/heatengine.py
+++ thermo/heatengine.py
@@ -4,4 +4,7 @@ from constants import constants
 from new_thermo import findTdwv, thetaep, tinvert_thetae, wsat, convertTempToSkew
 from convecSkew import convecSkew
 
-
+"""
+Keep track of equilibrium values as a heat engine expands
+along the length pvec.
+"""
