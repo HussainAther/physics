9ab3af29338c1aae0137c3e62c6a5d905322dc64
Syed Hussain Ather <shussainather@gmail.com>
2019-03-22 14:10:35 -0400

    np

--- fluid/diffusion.py
+++ fluid/diffusion.py
@@ -1,7 +1,7 @@
-import numpy
-from matplotlib import pyplot
+import numpy as np
 import time, sys
 
+from matplotlib import pyplot
 """
 
 The one-dimensional diffusion equation is:
@@ -49,14 +49,14 @@ interval 0.5≤x≤1 and u=1 everywhere else. We are ready to number-crunch!
 """
 
 
-u = numpy.ones(nx)
+u = np.ones(nx)
 u[int(.5 / dx):int(1 / dx + 1)] = 2
 
-un = numpy.ones(nx)
+un = np.ones(nx)
 
 for n in range(nt):
     un = u.copy() 
     for i in range(1, nx - 1):
         u[i] = un[i] + nu * dt / dx**2 * (un[i+1] - 2 * un[i] + un[i-1])
 
-pyplot.plot(numpy.linspace(0, 2, nx), u);
+pyplot.plot(np.linspace(0, 2, nx), u);
