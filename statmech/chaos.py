26e537181f7f3cc414b3dbfe23b33093b3a7f83b
Syed Hussain Ather <shussainather@gmail.com>
2019-03-24 21:11:43 -0400

    solve eig

--- statmech/chaos.py
+++ statmech/chaos.py
@@ -2,9 +2,17 @@ import numpy as np
 
 """
 We can use a discrete nonlinear logistic-type model in which we assume
-0 < u0 < 1 for
+0 < u_0 < 1 for
 
 u_t+1 = r * u_t * (1 - u_t) in which r > 0
 
 for rate r, population function u over time t. 
+
+The steady state solution and corresponding eiganvalues lambda are
+
+u_star = y0 
+lambda = f'(0) = r
+u_star = (r-1)/r 
+lambda = f'(ustar) = 2 - r
+
 """
