5a2a5b07a12a1a3c7603987a349e9588c61c9ccc
Syed Hussain Ather <shussainather@gmail.com>
2019-03-24 21:10:19 -0400

    initial commit

---
+++ statmech/chaos.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+"""
+We can use a discrete nonlinear logistic-type model in which we assume
+0 < u0 < 1 for
+
+u_t+1 = r * u_t * (1 - u_t) in which r > 0
+
+for rate r, population function u over time t. 
+"""
