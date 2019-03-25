9fc1abca839c887bef726d12d930ead830aeb9cd
Syed Hussain Ather <shussainather@gmail.com>
2019-03-24 21:23:43 -0400

    second iteration

--- statmech/chaos.py
+++ statmech/chaos.py
@@ -12,21 +12,29 @@ We can use a discrete nonlinear logistic-type model in which we assume
 
 u_t+1 = r * u_t * (1 - u_t) in which r > 0
 
-for rate r, population function u over time t. 
+for rate r, population function f(u) over time t. 
 
 The steady state solution and corresponding eiganvalues lambda are
 
 u_star = 0 # we define u_star as this 0 equilibrium of the function of population 
-lambda = f'(0) = r # r is the rate at t = 0. It's one of our solutions for lambda
+lambda = f'(0) = r # r is the rate of change of the function f at u = 0. It's one of our solutions for lambda
 u_star = (r-1)/r  # analytic solution
 lambda = f'(u_star) = 2 - r
 
 as r increases from 0 under the contsraint 0 < r < 1 
 with non-negative equilibrium u_star 
 """
-
-def ut2(u):
+# central differentiation
+def cd(y, t, h):
     """
-    For some function u that depends upon t, return the u_t+2 value
+    Differentiate around both sides of a point t of interval h.
+    """
+    return ( y(t+h/2) -y(t-h/2))/h
+
+def ut2(f, t):
+    """
+    For some function f that depends upon u that depends upon t, return the u_t+2 value
     from the second iteration as r passes through the bifurcation value.
     """
+    r = cd(u(f, 0, 5)) # over some interval t of size 5, differentiation the function f around u = 0
+    return r * (u(t) * (t-u(t)) * (1 - r*u(t) * (1 - u(t))))
