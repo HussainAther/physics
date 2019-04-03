a72bb1717622879dc15adc72ef6db130efcce491
Syed Hussain Ather <shussainather@gmail.com>
2019-04-03 00:54:34 -0400

    return update

--- thermo/belousovzhabotinsky.py
+++ thermo/belousovzhabotinsky.py
@@ -16,6 +16,7 @@ alpha, beta, gamma = 1, 1, 1 # rate constants
 def update(p, a):
     """
     Update an index p of an array a by evolving in time.
+    We can average neighbor concentrations through a convolution with a 3x3 array of 1/9 values.
     """
     # Count the average amount of each species in the 9 cells around each cell
     # by convolution with the 3x3 array m.
@@ -24,4 +25,8 @@ def update(p, a):
     m = np.ones((3,3)) / 9 # used for convolution
     for k in range(3): # for each reaction
         s[k] = convolve2d(a[p, k], m, mode="same", boundary="wrap") # same uses the same size for a. wrap indicates circular boundary conditions
-         
+    a[q,0] = s[0] + s[0]*(alpha*s[1] - gamma*s[2]) 
+    a[q,1] = s[1] + s[1]*(beta*s[2] - alpha*s[0]) 
+    a[q,2] = s[2] + s[2]*(gamma*s[0] - beta*s[1]) 
+    np.clip(a[q], 0, 1, arr[q]) # use [0,1] boundary conditions
+    return a
