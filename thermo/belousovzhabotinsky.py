a3033d048c9112f673c0fd90d6a1a1da95f32673
Syed Hussain Ather <shussainather@gmail.com>
2019-04-03 00:48:35 -0400

    init var

--- thermo/belousovzhabotinsky.py
+++ thermo/belousovzhabotinsky.py
@@ -9,3 +9,12 @@ The Belousov-Zhabotinsky (BZ bz belousov zhabotinsky) uses non-equilibrium chemi
 with periodic changes in concentration. We can create a simple reaction model using three
 chemical substrates with alpha, beta, and gamma rate constants.
 """
+
+nx, ny = 600, 450 # width and height of the image
+alpha, beta, gamma = 1, 1, 1 # rate constants
+
+def update(p,arr):
+    """Update arr[p] to arr[q] by evolving in time."""
+
+    # Count the average amount of each species in the 9 cells around each cell
+    # by convolution with the 3x3 array m.
