6d96b6657a736c1209a3cdd543c1bdc4c0c4a327
Syed Hussain Ather <shussainather@gmail.com>
2019-04-03 00:47:53 -0400

    import

--- thermo/belousovzhabotinsky.py
+++ thermo/belousovzhabotinsky.py
@@ -1,5 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.signal import convolve2d
+from matplotlib import animation
 
 """
-The Belousov-Zhabotinsky (BZ bz belousov zhabotinsky) uses non
+The Belousov-Zhabotinsky (BZ bz belousov zhabotinsky) uses non-equilibrium chemical oscillator
+with periodic changes in concentration. We can create a simple reaction model using three
+chemical substrates with alpha, beta, and gamma rate constants.
 """
