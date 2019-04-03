b7499915aff9d059bd73d54aca3efb8621b096d2
Syed Hussain Ather <shussainather@gmail.com>
2019-04-03 00:51:51 -0400

    convolve

--- thermo/belousovzhabotinsky.py
+++ thermo/belousovzhabotinsky.py
@@ -13,8 +13,15 @@ chemical substrates with alpha, beta, and gamma rate constants.
 nx, ny = 600, 450 # width and height of the image
 alpha, beta, gamma = 1, 1, 1 # rate constants
 
-def update(p,arr):
-    """Update arr[p] to arr[q] by evolving in time."""
-
+def update(p, a):
+    """
+    Update an index p of an array a by evolving in time.
+    """
     # Count the average amount of each species in the 9 cells around each cell
     # by convolution with the 3x3 array m.
+    q = (p+1) % 2  
+    s = np.zeros((3, ny,nx)) # discrete linear convolution of the array a with m
+    m = np.ones((3,3)) / 9 # used for convolution
+    for k in range(3): # for each reaction
+        s[k] = convolve2d(a[p, k], m, mode="same", boundary="wrap") # same uses the same size for a. wrap indicates circular boundary conditions
+         
