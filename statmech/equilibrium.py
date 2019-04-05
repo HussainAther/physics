f5a7b0ecb90267c752e1522b4d635cf1e183a569
Syed Hussain Ather <shussainather@gmail.com>
2019-04-04 21:53:25 -0400

    fix for loop

--- statmech/equilibrium.py
+++ statmech/equilibrium.py
@@ -8,9 +8,10 @@ Simple method of adjusting distances to achieve equilibrium.
 """
 
 for i in range(1,nstep):
-r = rand(1)
-if (r<n[i-1]/N):
-    n[i] = n[i-1] - 1 # Move atom from left to right else:
-    n[i] = n[i-1] + 1 # Move atom from right to left
-    plt(range(0,nstep),n/N, xlabel="t", ylabel="n/N")
-    plt.show()
+    r = rand(1)
+
+    if (r<n[i-1]/N):
+        n[i] = n[i-1] - 1 # Move atom from left to right else:
+        n[i] = n[i-1] + 1 # Move atom from right to left
+        plt(range(0,nstep),n/N, xlabel="t", ylabel="n/N")
+        plt.show()
