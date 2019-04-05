ee006ab4b91f40fe9a16a93f8bad30361261b6f6
Syed Hussain Ather <shussainather@gmail.com>
2019-04-04 21:38:30 -0400

    spacing

--- statmech/vanderwaals.py
+++ statmech/vanderwaals.py
@@ -12,5 +12,5 @@ t = [0.9,0.95,1.0,1.05] # adjusted temp
 for i in range(len(t)):
     T = t[i]
     V = np.linspace(0.5,2.0,1000)
-    p = 8.0/3.0*T/(V-1/3)-3.0/(V**2)
+    p = 8.0 / 3.0 * T / (V-1/3) - 3.0/ (V**2)
     plt(V,p xlabel="V/V_c", ylabel="p/p_c")
