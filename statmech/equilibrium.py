63f3553aaacf23d61d254dd384b9ad51cbd4bf8d
Syed Hussain Ather <shussainather@gmail.com>
2019-04-04 21:57:07 -0400

    init var

--- statmech/equilibrium.py
+++ statmech/equilibrium.py
@@ -7,10 +7,12 @@ import numpy as np
 Simple method of adjusting distances to achieve equilibrium.
 """
 
-for i in range(1,nstep):
-    r = rand(1)
+n = rand(20)
+N = 20
 
-    if (r<n[i-1]/N):
+for i in range(1,nstep):
+    r = rand(1) # create a single random value 
+    if (r < n[i-1]/N): # if we need to adjust distances
         n[i] = n[i-1] - 1 # Move atom from left to right else:
         n[i] = n[i-1] + 1 # Move atom from right to left
         plt(range(0,nstep),n/N, xlabel="t", ylabel="n/N")
