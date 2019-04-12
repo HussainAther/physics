30b42846af90f668b24bc3450132a525d3e839de
Syed Hussain Ather <shussainather@gmail.com>
2019-04-11 21:25:10 -0400

    syntax error

--- eandm/fdtdmaxwell.py
+++ eandm/fdtdmaxwell.py
@@ -59,7 +59,7 @@ while True:
     rate(600)
     Ex[1:xmax-1, 1] = Ex[1:xmax-1,0] + beta*(Hy[0:xmax-1,0]-Hy[2:xmax,0])
     Hy[1:xmax-1, 1] = Hy[1:xmax-1,0] + beta*(Ex[0:xmax-1,0]-Ex[2:xmax,0])
-    Ex[0,1] = Ex[0,]0 + beta*(Hy[xmax-2,0] - Hy[1,0]) # boundary condition
+    Ex[0,1] = Ex[0,0] + beta*(Hy[xmax-2,0] - Hy[1,0]) # boundary condition
     Ex[xmax-1, 1] = Ex[xmax-1, 0] + beta*(Hy[xmax-2,0] - Hy[1,0])
     Hy[0,1] = Hy[0,0] + beta*(Ex[xmax-2,0] - EX[1,0]) # boundary condition
     Hy[xmax-1, 1] = Hy[xmax-1, 0] + beta*(Ex[xmax-2,0] - Ex[1,0])
