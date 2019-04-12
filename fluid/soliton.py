0312befe0ffeff3d5a5995ffb30999bfd299d055
Syed Hussain Ather <shussainather@gmail.com>
2019-04-11 22:07:42 -0400

    syntax comma

--- fluid/soliton.py
+++ fluid/soliton.py
@@ -46,7 +46,7 @@ for j in range(1, max+1):
     for i in range(1, mx-2):
         a1 = eps*dt*(u[i+1, 1] + u[i, 1] + u[i-1, 1])/(3*ds)
         if i > 1 and i < mx-2:
-            a2 = u[i+2, 1] + 2*u[i-1, 1] - 2*u[i+1,,1] - u[i-2,1]
+            a2 = u[i+2, 1] + 2*u[i-1, 1] - 2*u[i+1,1] - u[i-2,1]
         else:
             a2 = u[i-1, 1] - u[i+1, 1]
         a3 = u[i+1, 0] - u[i-1, 0]
