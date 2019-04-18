175c06141b5e115e87adc4ccf314a039456686a2
Syed Hussain Ather <shussainather@gmail.com>
2019-04-18 14:56:21 -0400

    indent

--- eandm/circuit.py
+++ eandm/circuit.py
@@ -635,8 +635,7 @@ class RangeNode(AVLNode):
 
     def __init__(self, key):
         """
-        Creates a node that will be inserted in a range index tree.
-  
+        Create a node that will be inserted in a range index tree.
         See __init__ in BSTNode.
         """
         AVLNode.__init__(self, key)
@@ -644,43 +643,43 @@ class RangeNode(AVLNode):
 
     def update_subtree_info(self):
         """
-        Updates pre-computed fields such as the node"s subtree height.
+        Update pre-computed fields such as the node's subtree height.
         """
         AVLNode.update_subtree_info(self)
         self.tree_size = self._uncached_tree_size()
 
     def _uncached_tree_size(self):
         """
-        Re-computes the node"s subtree size based on the children's heights.
+        Re-compute the node's subtree size based on the children's heights.
         """
         return 1 + (((self.left and self.left.tree_size) or 0) +
                 ((self.right and self.right.tree_size) or 0))
     
-  def check_ri(self):
-    """Checks the AVL representation invariant around this node.
-    
-    Raises an exception if the RI is violated.
-    """
-    if self.tree_size != self._uncached_tree_size():
-      raise RuntimeError("RangeTree RI violated by node subtree size")
-    AVLNode.check_ri(self)
+    def check_ri(self):
+        """
+        Checks the AVL representation invariant around this node.
+        Raises an exception if the RI is violated.
+        """
+        if self.tree_size != self._uncached_tree_size():
+            raise RuntimeError("RangeTree RI violated by node subtree size")
+        AVLNode.check_ri(self)
 
-  def rank(self, key):
-    """
-    Number of keys <= the given key in the subtree rooted at this node.
-    """
-    if key < self.key:
-      if self.left is not None:
-        return self.left.rank(key)
-      else:
-        return 0
-    if self.left:
-      lrank = 1 + self.left.tree_size
-    else:
-      lrank = 1
-    if key > self.key and self.right is not None:
-      return lrank + self.right.rank(key)
-    return lrank
+    def rank(self, key):
+        """
+        Number of keys <= the given key in the subtree rooted at this node.
+        """
+        if key < self.key:
+            if self.left is not None:
+                return self.left.rank(key)
+            else:
+                return 0
+        if self.left:
+            lrank = 1 + self.left.tree_size
+        else:
+            lrank = 1
+        if key > self.key and self.right is not None:
+            return lrank + self.right.rank(key)
+        return lrank
 
   def lca(self, low_key, high_key):
     """Lowest-common ancestor node of nodes with low_key and high_key.
