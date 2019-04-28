6ea6a4e61c237435f1655858e87552c0067cb9db
Syed Hussain Ather <shussainather@gmail.com>
2019-04-27 20:11:12 -0400

    def

--- thermo/chaos.py
+++ thermo/chaos.py
@@ -1,4 +1,14 @@
+import numpy as np
+
 """
-We obserev chaos when we split separatrices of Hamiltonian systems in homo-
+We observe chaos when we split separatrices of Hamiltonian systems in homo-
 and heteroclinic orbits.
 """
+
+def eq(t, x):
+    """
+    An equation of motion for which we will find the coresponding Hamiltonian
+    vector field.
+    """
+
+dx2d2t + (omega**2 + epsilon * np.cos(t) * np.sin(x) = 0 
