4149d514d21df7b150aa577f4f6e911abeb242a2
Syed Hussain Ather <shussainather@gmail.com>
2019-04-27 20:46:16 -0400

    response

--- eandm/vanderpol.py
+++ eandm/vanderpol.py
@@ -1,10 +1,15 @@
 import numpy as np
 
 """
-We can create electrical circuits governed by the Van der Pol oscillator equation:
+We can create electrical circuits governed by the van der Pol oscillator equation:
 
 dx2/d2t - epsilon(1-x^2)dx/dt + x = 0
 
 in which epsilon is a parameter > 0 for some dynamical variable x.
-
 """
+
+def response(x, t):
+    """
+    The van der Pol oscillator responds to periodic forcing with two frequencies
+    in the system: frequency of self-oscillation and frequency of periodic forcing. 
+    """
