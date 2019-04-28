ae9eae6b28fc7e60c0f63bdab407d0d4e490ddec
Syed Hussain Ather <shussainather@gmail.com>
2019-04-27 22:25:49 -0400

    bg and def

--- eandm/chua.py
+++ eandm/chua.py
@@ -1,3 +1,21 @@
+import numpy as np
+
 """
-The simplest electric circuit that exhibits chaos is the Chua circuit.
+The simplest electric circuit that exhibits chaos is the Chua (chua Chua's) circuit.
+It produces a waveform that never repeats itself. It's a real-world example of chaotic
+behavior.
 """
+
+def res(x):
+    """
+    Nonlinear resistor response to voltage x(t)
+    """
+    return np.sin(x) 
+
+def ChuaEq(alpha, beta, x, y, z):
+    """
+    For x(t), y(t), and z(t) voltages across capacitors C1 and C2 and current in
+    inductor L1, respectively, we describe three equations to illustrate the 
+    dynamics of Chua's circuit.
+    """
+    dxdt = alpha*(y - x -res(x))
