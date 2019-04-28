fcf8ce823ea680aba761369f9a963e723c767dff
Syed Hussain Ather <shussainather@gmail.com>
2019-04-27 20:44:53 -0400

    eq

--- eandm/vanderpol.py
+++ eandm/vanderpol.py
@@ -1,3 +1,10 @@
+import numpy as np
+
 """
-We can create electrical circuits governed by the Van der Pol oscillator.
+We can create electrical circuits governed by the Van der Pol oscillator equation:
+
+dx2/d2t - epsilon(1-x^2)dx/dt + x = 0
+
+in which epsilon is a parameter > 0 for some dynamical variable x.
+
 """
