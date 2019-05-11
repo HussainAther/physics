db3f8a5478fe9986d88b821f99b31dc277cbbb33
Syed Hussain Ather <shussainather@gmail.com>
2019-05-11 02:07:34 -0400

    def

--- fluid/fordfulkerson.py
+++ fluid/fordfulkerson.py
@@ -4,3 +4,9 @@ start with some flow function (initially this might consist of zero flow on ever
 Then look for a flow augmenting path in the network. A flow augmenting path is a path 
 from the source to the sink along which we can push some additional flow.
 """
+
+def fofu(graph, source, sink):
+    """
+    Ford-Fulkerson (ford Ford fulkerson Fulkerson) to serach for a path between source
+    and sink.
+    """
