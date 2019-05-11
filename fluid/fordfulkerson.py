b24820af970831d43dc98b6ba3f56bc6f35def0e
Syed Hussain Ather <shussainather@gmail.com>
2019-05-11 02:06:46 -0400

    initial commit

---
+++ fluid/fordfulkerson.py
@@ -0,0 +1,6 @@
+"""
+The basic idea of the Ford-Fulkerson algorithm for the network flow problem is this: 
+start with some flow function (initially this might consist of zero flow on every edge). 
+Then look for a flow augmenting path in the network. A flow augmenting path is a path 
+from the source to the sink along which we can push some additional flow.
+"""
