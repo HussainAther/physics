59220524e07665c5855e36955da2b23b679b78aa
Syed Hussain Ather <shussainather@gmail.com>
2019-05-14 17:37:49 -0400

    def

--- statmech/entropynarcissist.py
+++ statmech/entropynarcissist.py
@@ -1,3 +1,18 @@
+import math
+from collections import Counter
+ 
 """
 A program that compute its own entropy.
 """
+
+def entropy(s):
+    """
+    For some input value of text s, calculate entropy.
+    """
+    p, lns = Counter(s), float(len(s))
+    return -sum( count/lns * math.log(count/lns, 2) for count in p.values())
+ 
+with open(__file__) as f: # Open this file itself
+    b=f.read() # Read this file
+ 
+print(entropy(b)) # Calculate the entropy of this file
