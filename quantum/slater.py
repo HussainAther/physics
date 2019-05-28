5c2347727c59bb4d7a85719f3e3a2db522b7da12
Syed Hussain Ather <shussainather@gmail.com>
2019-05-28 12:50:46 -0400

    Def ham

--- quantum/slater.py
+++ quantum/slater.py
@@ -10,3 +10,6 @@ p, q, r, s = symbols("p q r s", dummy=True)
 f = AntiSymmetricTensor("f", (p,), (q,))
 pr = Fd(p) * F(q)
 v = AntiSymmetricTensor("v", (p, q), (r, s))
+pqsr = Fd(p) * Fd(q) * F(s) * F(r)
+Hamiltonian = f * pr + Rational(1) / Rational(4) * v * pqsr
+
