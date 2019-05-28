3a033be927b83aef1a5c1d1487b8a600ea952c2d
Syed Hussain Ather <shussainather@gmail.com>
2019-05-28 12:33:58 -0400

    Basis

--- quantum/exactdiagonalization.cpp
+++ quantum/exactdiagonalization.cpp
@@ -23,3 +23,15 @@ private:
 
 /* Fermion basis states */
 FermionBasis::FermionBasis(int L, int N)
+{
+    index_.resize(1<<L); // 2^L entries
+    for (state_type s=0;s<index_.size();++s)
+        if(alps::popcnt(s)==N) {
+            // correct number of particles
+            states_.push_back(s);
+            index_[s]=states_.size()-1;
+}
+    else
+        // invalid state
+        index_[s]=std::numeric_limits<index_type>::max();
+}
