baadbd14139c8eac7109f12c59ecf2787b0eae7f
Syed Hussain Ather <shussainather@gmail.com>
2019-05-28 12:52:06 -0400

    print

--- quantum/slater.py
+++ quantum/slater.py
@@ -13,3 +13,9 @@ v = AntiSymmetricTensor("v", (p, q), (r, s))
 pqsr = Fd(p) * Fd(q) * F(s) * F(r)
 Hamiltonian = f * pr + Rational(1) / Rational(4) * v * pqsr
 a, b, c, d, e, f = symbols("a, b, c, d, e, f", above_fermi=True)
+
+# Create teh representatoin
+expression = wicks(F(c) * F(b) * F(a) * Hamiltonian * Fd(d) * Fd(e) * Fd(f), keep_only_fully_contracted=True, simplify_kronecker_deltas=True)
+expression = evaluate_deltas(expression)
+expression = simplify(expression)
+print(latex(expression))
