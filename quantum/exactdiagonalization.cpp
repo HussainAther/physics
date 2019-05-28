3d1a9eb47827ebc30439681965d81d76cc180dc5
Syed Hussain Ather <shussainather@gmail.com>
2019-05-28 12:30:26 -0400

    bg

--- quantum/exactdiagonalization.cpp
+++ quantum/exactdiagonalization.cpp
@@ -3,3 +3,10 @@
 #include <limits>
 #include <valarray>
 #include <cassert>
+
+/* Exact diagonalization of the Hamiltonian matrix using the Lanczos algorithm.*/ 
+
+class FermionBasis {
+public:
+typedef unsigned int state_type;
+typedef unsigned int index_type;
