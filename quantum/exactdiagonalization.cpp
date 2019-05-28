a7c31d057c185e6eb33427e86fa2e9c60e464f2e
Syed Hussain Ather <shussainather@gmail.com>
2019-05-28 12:32:37 -0400

    private

--- quantum/exactdiagonalization.cpp
+++ quantum/exactdiagonalization.cpp
@@ -10,5 +10,14 @@ The size of the Hilbert space of an N-site system can be reduced using symmetrie
 
 class FermionBasis {
 public:
-typedef unsigned int state_type;
-typedef unsigned int index_type;
+    typedef unsigned int state_type;
+    typedef unsigned int index_type;
+    FermionBasis (int L, int N);
+    state_type state(index_type i) const {return states_[i];}
+    index_type index(state_type s) const {return index_[s];}
+    unsigned int dimension() const { return states_.size();}
+private:
+    std::vector<state_type> states_;
+    std::vector<index_type> index_;
+};
+I
