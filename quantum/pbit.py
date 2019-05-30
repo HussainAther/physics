da03c7de7dfdc9a562c79c514b177758480827bf
Syed Hussain Ather <shussainather@gmail.com>
2019-05-30 09:49:23 -0400

    comment

--- quantum/pbit.py
+++ quantum/pbit.py
@@ -2,10 +2,13 @@ import numpy as np
 
 """
 Weighted p-bit uesd in p-circuits and p-computing (p bit pbit pcircuit p circuit pcomputing p computing).
+Probabilistic computers (p-computers pcomputers p computers) can operate
+at room temperature with existing technology on p-bits that change between
+0 and 1. We use an input with stochastic output.
 """
 
 def Ii():
     """
-    Weighted p-bit integrating relevant parts of the synapse onto the 
+    Response to weighted p-bit integrating relevant parts of the synapse onto the 
     neurons.
     """
