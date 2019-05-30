61d717b0c7a83ea6ec5dd50867878aed2fdbce26
Syed Hussain Ather <shussainather@gmail.com>
2019-05-30 09:46:07 -0400

    def

--- quantum/pbit.py
+++ quantum/pbit.py
@@ -1,3 +1,11 @@
+import numpy as np
+
 """
 Weighted p-bit uesd in p-circuits and p-computing (p bit pbit pcircuit p circuit pcomputing p computing).
 """
+
+def Ii():
+    """
+    Weighted p-bit integrating relevant parts of the synapse onto the 
+    neurons.
+    """
