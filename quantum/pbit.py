5346d97663b88c44436bd62238e1e007dd9d75c9
Syed Hussain Ather <shussainather@gmail.com>
2019-05-30 09:56:33 -0400

    current return

--- quantum/pbit.py
+++ quantum/pbit.py
@@ -7,8 +7,16 @@ at room temperature with existing technology on p-bits that change between
 0 and 1. We use an input with stochastic output.
 """
 
-def Ii():
+def Ii(C0, C, V0, Vout):
     """
-    Response to weighted p-bit integrating relevant parts of the synapse onto the 
-    neurons.
+    Current response to weighted p-bit integrating relevant parts of the synapse onto the 
+    neurons for C0 input capacitance of the transistor, C is an array of the capacitances
+    of each node, V0 is the voltage of the transistor, and Vout is the output voltage
+    at each node.
     """
+    summ = 0
+    for i in C:
+        num = i * Vout[i] 
+        den = V0 * (C0 + sum(C))
+        summ += num/den
+    return summ 
