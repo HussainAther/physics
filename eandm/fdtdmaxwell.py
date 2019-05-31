539842e914ba494358346da11e27a5218a8f26fe
Syed Hussain Ather <shussainather@gmail.com>
2019-05-30 21:28:44 -0400

    no import *

--- eandm/fdtdmaxwell.py
+++ eandm/fdtdmaxwell.py
@@ -1,6 +1,5 @@
 import numpy as np
-
-from vpython import *
+import vpython as vp
 
 """
 Finite-Difference Time Domain Method of findings solutions to Maxwell's equations for linearly
@@ -11,16 +10,16 @@ xmax = 201
 ymax = 100
 zmax = 100
 
-scene = display(x=0, y=0, width=800, height=500, title="E: cyan, H: red. Periodic Boundary conditions",
+scene = vp.display(x=0, y=0, width=800, height=500, title="E: cyan, H: red. Periodic Boundary conditions",
             forward=(-.6,-.5,-1))
-Efield = curve(x=list(range(0, xmax), color=color.cyan, radius=1.5, display=scene))
-Hfield = curve(x=list(range(0, xmax), color=color.cyan, radius=1.5, display=scene))
-vplane = curve(pos=[(-xmax, ymax), (xmax, ymax), (xmax, -ymax), (-xmax, -ymax),
+Efield = vp.curve(x=list(range(0, xmax), color=color.cyan, radius=1.5, display=scene))
+Hfield = vp.curve(x=list(range(0, xmax), color=color.cyan, radius=1.5, display=scene))
+vplane = vp.curve(pos=[(-xmax, ymax), (xmax, ymax), (xmax, -ymax), (-xmax, -ymax),
                     (-xmax, ymax)], color=color.cyan)
-zaxis = curve(pos=[(-xmax, 0), (xmax, 0)], color=color.magenta)
-hplace = curve(pos=[(-xmax, 0, zmax), (xmax, 0, zmax), (xmax, 0, -zmax), (-xmax, 0, -zmax),
+zaxis = vp.curve(pos=[(-xmax, 0), (xmax, 0)], color=color.magenta)
+hplace = vp.curve(pos=[(-xmax, 0, zmax), (xmax, 0, zmax), (xmax, 0, -zmax), (-xmax, 0, -zmax),
                     (-xmax,0, zmax)], color=color.magenta)
-ball1 = sphere(pos =(xmax+30, 0, 0), color=color.black, radius=2)
+ball1 = vp.sphere(pos =(xmax+30, 0, 0), color=color.black, radius=2)
 ts = 2
 beta = .01
 
