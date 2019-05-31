b8902e79effaf52f326ec5641f3ffa03fe4509d5
Syed Hussain Ather <shussainather@gmail.com>
2019-05-31 14:25:02 -0400

    bg

--- thermo/cfl.py
+++ thermo/cfl.py
@@ -1,3 +1,4 @@
 """
-Compute the Courant-Friedrichs-Loewy coefficient (Courant Friedrichs Loewy cfl). 
+Compute the Courant-Friedrichs-Loewy coefficient (Courant Friedrichs Loewy cfl) for explicit heat
+diffusion. We use this to solve second-order differential equations. 
 """
