34506fd8500add0a4a7ccc7b12ff4f4afd9a8555
Syed Hussain Ather <shussainather@gmail.com>
2019-05-31 14:29:45 -0400

    return

--- thermo/cfl.py
+++ thermo/cfl.py
@@ -5,9 +5,11 @@ diffusion. We use this to solve second-order differential equations.
 
 def calccfl(k, t, x):
     """
-    Use the finite difference method to approximate the second derivative in space. Use a forward
+    Use the finite difference method to approximate the second derivative in space to calculate CFL. Use a forward
     Euler approximation to the first derivative in time. Uses the real heat conductivity coefficient k,
     time array t, and position array x.
     """
     xstep = (max(x) - min(x)) / (len(x)-1)
     tstep = (max(t) - min(t)) / (len(t)-1)
+    cfl = k*tstep/xstep**2
+    return cfl
