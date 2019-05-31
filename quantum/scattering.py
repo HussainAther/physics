af15ecc4bb644f93254b77b17e22fad92b5d9113
Syed Hussain Ather <shussainather@gmail.com>
2019-05-30 21:16:58 -0400

    no import *

--- quantum/scattering.py
+++ quantum/scattering.py
@@ -1,8 +1,6 @@
-from vpython.graph import *
-
 import gauss # using gauss.pyc
-import numpy.linalg as lina
 import numpy as np
+import vpython as vp
 
 """
 Solve the Lippmann-Schwinger integral equation for scattering from delta-shell potential.
@@ -10,7 +8,7 @@ The singular integral equations are regularized by subtraction, converted to mat
 Gaussian grid points, and then solved with matrix library routines.
 """
 
-graphscatt = gidsplay(x=0, y=0, xmin=0, xmax=6, ymin=0, ymax=1, width=600, height=400, title="S wave cross section vs. E", xtitle="kb", ytitle="[sin(delta])**2")
+graphscatt = vp.graph.gidsplay(x=0, y=0, xmin=0, xmax=6, ymin=0, ymax=1, width=600, height=400, title="S wave cross section vs. E", xtitle="kb", ytitle="[sin(delta])**2")
 sin2plot = gcurve(color=color.yellow)
 
 M = 27
@@ -33,7 +31,7 @@ ko = .02
 for m in range(1, 901):
     k[n] = ko
     for i in range (0, n):
-        D[i]=2/pi*w[i]*k[i]*k[i]/(k[i]*k[i]-ko*ko) #D
+        D[i]=2/np.pi*w[i]*k[i]*k[i]/(k[i]*k[i]-ko*ko) #D
     D[n] = 0.
     for  j in range(0,n):
         D[n]=D[n]+w[j]*ko*ko/(k[j]*k[j]-ko*ko)
@@ -41,17 +39,17 @@ for m in range(1, 901):
     for i in range(0,n+1):
         for j in range(0,n+1):
             # Set up F matrix and V vector
-            pot = -b*b * lambd * sin(b*k[i])*sin(b*k[j])/(k[i]*b*k[j]*b)
+            pot = -b*b * lambd * np.sin(b*k[i])*np.sin(b*k[j])/(k[i]*b*k[j]*b)
             F[i][j] = pot*D[j] # Form F
             if i==j:
                 F[i][j] = F[i][j] + 1.
-    V[i] = pot  # Define V
+    V[i] = pot # Define V potential
     for  i in range(0,n+1):
         Vvec[i][0]= V[i]
-    Finv = lina.inv(F)
-    R = dot(Finv, Vvec)
+    Finv = np.linalg.inv(F)
+    R = np.dot(Finv, Vvec)
     RN1 = R[n][0]
-    shift = atan(-RN1*ko)
-    sin2 = (sin(shift))**2
+    shift = np.atan(-RN1*ko)
+    sin2 = (np.sin(shift))**2
     sin2plot.plot(pos = (ko*b,sin2))
     ko = ko + 0.2*pi/1000.
