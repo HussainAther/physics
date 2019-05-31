89b6d1a9eb017e4b2770c456f92036b2d3f1dd7a
Syed Hussain Ather <shussainather@gmail.com>
2019-05-30 21:52:16 -0400

    no import *

--- mech/linearize.py
+++ mech/linearize.py
@@ -1,5 +1,5 @@
-from sympy import *
-from sympy.physics.mechanics import *
+import sympy as sm
+import symp.physics.mechanics as smpm
 
 """
 In this example we demonstrate the use of the functionality provided in mechanics for
@@ -10,14 +10,14 @@ to describe the system using the x and y coordinates of the mass gives constrain
 Initialize KanesMethod object and use kanes_equation class method linearization.
 """
 
-q1 = dynamicsymbols("q1") # Pendulum angle
-u1 = dynamicsymbols("u1") # Anguluar velocity
-q1d = dynamicsymbols("q1", 1)
-L, m, t, g = symbols("L, m, t, g")
+q1 = smpm.dynamicsymbols("q1") # Pendulum angle
+u1 = smpm.dynamicsymbols("u1") # Anguluar velocity
+q1d = smpm.dynamicsymbols("q1", 1)
+L, m, t, g = sm.symbols("L, m, t, g")
 
 # World frame
-N = ReferenceFrame("N")
-pN = Point("N*")
+N = smpm.ReferenceFrame("N")
+pN = smpm.Point("N*")
 pN.set_vel(N, 0)
 
 # A.x is along the pendulum
@@ -30,19 +30,19 @@ vel_P = P.v2pt_theory(pN, N, A)
 pP = Particle("pP", P, m)
 
 # Create Kinematic Differential Equations
-kde = Matrix([q1d - u1])
+kde = sm.Matrix([q1d - u1])
 
 # Input force at P
 R = m*g*N.x
 
 # Solve for eom with KanesMethod
-KM = KanesMethod(N, q_ind=[q1], u_ind=[u1], kd_eqs=kde)
+KM = smpm.KanesMethod(N, q_ind=[q1], u_ind=[u1], kd_eqs=kde)
 fr, frstar = KM.kanes_equations([pP], [P, R])
-
 
 """
 Linear Lagrange's Equations
 """
+
 A = N.orientnew("A", "axis", [q1, N.z])
 A.set_ang_vel(N, q1d*N.z)
 P = pN.locatenew("P", L*A.x)
@@ -50,6 +50,6 @@ vel_P = P.v2pt_theory(pN, N, A)
 pP = Particle("pP", P, m)
 
 # Solve for eom with Lagrange's method
-Lag = Lagrangian(N, pP)
-LM = LagrangesMethod(Lag, [q1], forcelist=[(P, R)], frame=N)
+Lag = smpm.Lagrangian(N, pP)
+LM = smpm.LagrangesMethod(Lag, [q1], forcelist=[(P, R)], frame=N)
 lag_eqs = LM.form_lagranges_equations()
