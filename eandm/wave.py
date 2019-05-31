282e60e54ef147cebfbaff426b0ed9a48f2eca5c
Syed Hussain Ather <shussainather@gmail.com>
2019-05-30 21:31:20 -0400

    no import *

--- eandm/wave.py
+++ eandm/wave.py
@@ -1,26 +1,25 @@
-import math
-
-from vpython import *
+import numpy as np
+import vpython as vp
 
 """
 Plot the sine and cosine of the waves in 3-D
 """
 
 xmax = 201
-scene = display(x=0, y=0, width=500, height=500,
+scene = vp.display(x=0, y=0, width=500, height=500,
     title="sin(6pi*x/201-t)", background=(1.,1.,1.),
     forward=(-0.6,-0.5,1))
 
-sinWave = curve(x=range(0, xmax), color=color.yellow, radius=4.5)
-cosWave = curve(x=range(0, xmax), color=color.red, radius=4.5)
-Xaxis = curve(pos=[(-300, 0, 0), (300, 0, 0)], color=color.blue)
+sinWave = vp.curve(x=range(0, xmax), color=color.yellow, radius=4.5)
+cosWave = vp.curve(x=range(0, xmax), color=color.red, radius=4.5)
+Xaxis = vp.curve(pos=[(-300, 0, 0), (300, 0, 0)], color=color.blue)
 
-incr = math.pi/xmax # x increment
+incr = np.pi/xmax # x increment
 for t in range(0, 10, .02): # time loop
     for i in range(0, xmax): # loop through x values
         x = i*incr
-        f = math.sin(6.0*x-t)
-        zz = math.cos(6.0*x-t)
+        f = np.sin(6.0*x-t)
+        zz = np.cos(6.0*x-t)
         yp = 100*f
         xp = 200*x-300
         zp = 100*zz
