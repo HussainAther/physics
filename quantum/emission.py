6002bff38c03c2743b81be2f1ab82c7e4436b9f2
Syed Hussain Ather <shussainather@gmail.com>
2019-05-30 21:04:29 -0400

    no import *

--- quantum/emission.py
+++ quantum/emission.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pylab as plt
-
-from qutip import *
+import qutip as qt
 
 """
 Given a correlation function ⟨𝐴(𝜏 )𝐵(0)⟩ we can define the corresponding power spectrum as
@@ -21,8 +20,8 @@ g = 0.1 * 2 * np.pi # coupling strength
 kappa = 0.75 # cavity dissipation rate
 gamma = 0.25 # atom dissipation rate
 # Jaynes-Cummings Hamiltonian
-a = tensor(destroy(N), qeye(2))
-sm = tensor(qeye(N), destroy(2))
+a = qt.tensor(destroy(N), qeye(2))
+sm = qt.tensor(qeye(N), destroy(2))
 H = wc * a.dag() * a + wa * sm.dag() * sm + g * (a.dag() * sm + a * sm.dag())
 # collapse operators
 n_th = 0.25
@@ -35,20 +34,20 @@ function for a sufficient long time and sufficiently high sampling rate so
 that the discrete Fourier transform (FFT) captures all the features in the
 """
 
-# resulting spectrum.
+# resulting spectrum
 tlist = np.linspace(0, 100, 5000)
 corr = correlation_ss(H, tlist, c_ops, a.dag(), a)
-wlist1, spec1 = spectrum_correlation_fft(tlist, corr)
+wlist1, spec1 = qt.spectrum_correlation_fft(tlist, corr)
 
 # calculate the power spectrum using spectrum, which internally uses essolve
 # to solve for the dynamics (by default)
 wlist2 = np.linspace(0.25, 1.75, 200) * 2 * np.pi
-spec2 = spectrum(H, wlist2, c_ops, a.dag(), a)
+spec2 = qt.spectrum(H, wlist2, c_ops, a.dag(), a)
 
 # plot the spectra
 fig, ax = plt.subplots(1, 1)
-ax.plot(wlist1 / (2 * np.pi), spec1, "b", lw=2, label='eseries method')
-ax.plot(wlist2 / (2 * np.pi), spec2, "r--", lw=2, label='me+fft method')
+ax.plot(wlist1 / (2 * np.pi), spec1, "b", lw=2, label="eseries method")
+ax.plot(wlist2 / (2 * np.pi), spec2, "r--", lw=2, label="me+fft method")
 ax.legend()
 ax.set_xlabel("Frequency")
 ax.set_ylabel("Power spectrum")
