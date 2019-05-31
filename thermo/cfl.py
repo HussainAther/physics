3013f0baac7a4dfdaf13f52ebf085cf29973b3e7
Syed Hussain Ather <shussainather@gmail.com>
2019-05-31 14:27:52 -0400

    Def

--- thermo/cfl.py
+++ thermo/cfl.py
@@ -2,3 +2,12 @@
 Compute the Courant-Friedrichs-Loewy coefficient (Courant Friedrichs Loewy cfl) for explicit heat
 diffusion. We use this to solve second-order differential equations. 
 """
+
+def calccfl(k, t, x):
+    """
+    Use the finite difference method to approximate the second derivative in space. Use a forward
+    Euler approximation to the first derivative in time. Uses the real heat conductivity coefficient k,
+    time array t, and position array x.
+    """
+    xstep = (max(x) - min(x)) / (len(x)-1)
+    tstep = (max(t) - min(t)) / (len(t)-1)
