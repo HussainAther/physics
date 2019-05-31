3598bd0e16b9a2ecaf4622587dacdb53001e47d7
Syed Hussain Ather <shussainather@gmail.com>
2019-05-31 10:40:34 -0400

    init commit

---
+++ eandm/huckel.py
@@ -0,0 +1,10 @@
+"""
+The (Huckel) Hückel method or Hückel molecular orbital method (HMO), proposed by Erich Hückel 
+in 1930, is a very simple linear combination of atomic orbitals molecular orbitals (LCAO MO) 
+method for the determination of energies of molecular orbitals of pi electrons in conjugated 
+hydrocarbon systems, such as ethene, benzene and butadiene. It is the theoretical basis for 
+the Hückel's rule. The extended Hückel method developed by Roald Hoffmann is computational 
+and three-dimensional and was used to test the Woodward–Hoffmann rules. It was later extended 
+to conjugated molecules such as pyridine, pyrrole and furan that contain atoms other than carbon, 
+known in this context as heteroatoms.
+"""
