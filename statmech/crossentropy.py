358726487fb8b8439f0a7d1d55363f8bc6a6a166
Syed Hussain Ather <shussainather@gmail.com>
2019-06-10 12:02:35 -0400

    code

--- statmech/crossentropy.py
+++ statmech/crossentropy.py
@@ -1,4 +1,7 @@
+import numpy as np
+
 """
+Cross entropy as a measure of optimizing a convolutional neural network.
 """
 
 def softmax(X):
@@ -8,3 +11,15 @@ def softmax(X):
     """
     exps = np.exp(X)
     return exps / np.sum(exps)
+
+def cross_entropy(X,y):
+    """
+    Return cross entropy, distance between what the model believes a distribution
+    is and what the distribution really is. X is the output from fully connected 
+    layer (num_examples x num_classes). y is labels (num_examples x 1)
+    """
+    m = y.shape[0]
+    p = softmax(X)
+    log_likelihood = -np.log(p[range(m),y])
+    loss = np.sum(log_likelihood) / m
+    return loss
