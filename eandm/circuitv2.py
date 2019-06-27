387150d86b15efee6fa2985c24669310c8fa1f81
Syed Hussain Ather <shussainather@gmail.com>
2019-06-27 18:14:44 -0400

    print

--- eandm/circuitv2.py
+++ eandm/circuitv2.py
@@ -29,3 +29,10 @@ I1 = I2*(R2 + RL + 1j*w*L2 + R + 1j*w*L)/(1j*w*M + R + 1j*w*L)
 # Reverse direction
 I2r = E1/((r + R1 + 1j*w*L1 + R + 1j*w*L)*(R2 + RL + 1j*w*L2 + R + 1j*w*L)/(-1*1j*w*M + R + 1j*w*L) + (-1*(-1*1j*w*M + R + 1j*w*L)))
 I1r = I2r*(R2 + R + 1j*w*L2 + R + 1j*w*L)/(-1*1j*w*M + R + 1j*w*L)
+
+# Results
+print("\n\n  Result  \n\n")
+print("primary  current  I1  is  ",round(abs(I1),2),"/_",round(cmath.phase(complex(I1.real,I1.imag))*180/math.pi,2),"deg  A")
+print("load  current  I2  is  ",round(abs(I2),2),"/_",round(cmath.phase(complex(I2.real,I2.imag))*180/math.pi,2),"deg   A")
+print("reversed  primary  current  I1r  is  ",round(abs(I1r),2),"/_",round(cmath.phase(complex(I1r.real,I1r.imag))*180/math.pi,2),"deg   A")
+print("reversed  load  current  I2r  is  ",round(abs(I2r),2),"/_",round(cmath.phase(complex(I2r.real,I2r.imag))*180/math.pi,2),"deg   A")
