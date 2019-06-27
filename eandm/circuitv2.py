d71e07136be70ff909248a902776127d1b455cbb
Syed Hussain Ather <shussainather@gmail.com>
2019-06-27 18:10:23 -0400

    w

--- eandm/circuitv2.py
+++ eandm/circuitv2.py
@@ -6,15 +6,18 @@ More circuit stuff.
 """
 
 # Initialize.
-E1  =  50 # in Volts
+E1 = 50 # in Volts
 thetae1  = 0 # in degrees
-r  =  5 # in ohm
-R1  =  20 # in ohm
-L1  =  0.2 # in Henry
-R  =  8 # in ohm
-L  =  0.1 # in Henry
-L2  =  0.4 # in Henry
-R2  =  25 # in ohm
-RL  =  20 # in ohm
-M  =  0.1 # in Henry
-f  =  75/np.pi # in Hz
+r = 5 # in ohm
+R1 = 20 # in ohm
+L1 = 0.2 # in Henry
+R = 8 # in ohm
+L = 0.1 # in Henry
+L2 = 0.4 # in Henry
+R2 = 25 # in ohm
+RL = 20 # in ohm
+M = 0.1 # in Henry
+f = 75/np.pi # in Hz
+
+# Kirchhoff's law (kirchhoff) to primary circuit
+w = 2*np.pi*f
