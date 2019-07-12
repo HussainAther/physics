73d404b8511ae0b95abca31f236887a3066a03af
Syed Hussain Ather <shussainather@gmail.com>
2019-07-11 20:01:36 -0400

    init

---
+++ mech/fallingball.py
@@ -0,0 +1,8 @@
+
+
+"""
+This program uses the Euler method to solve for the motion of a
+ball dropped from rest close to the surface of Earth . The
+program depends on the function VelocityLinearDrag to execute
+the Euler method.
+"""
