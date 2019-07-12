68f562b00e282902d9318d9755c750607d21cda8
Syed Hussain Ather <shussainather@gmail.com>
2019-07-11 20:05:04 -0400

    parser

--- mech/fallingball.py
+++ mech/fallingball.py
@@ -17,3 +17,8 @@ g = 9.8
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--filename", dest="filename", action="store", help="output filename", default=junk.dat")
+parser.add_argument("--v_initial", dest="v", type=float, default=0, help="initial velocity")
+parser_add_argument("--v_terminal", dest="vterminal", type=float, default=2, help="terminal velocity")
+parser.add_argument("--tmax", dest="tmax", type=float, default=.2, hel="maximum simulation time")
+parser.add_argument("--df", dest="dt", help="time steps", action="append")
+parser.add_argument("--savePlot", dest="savePlot", action="store", default="none", help="Save a hardcopy of plot? (specifify .pdf or .png)")
