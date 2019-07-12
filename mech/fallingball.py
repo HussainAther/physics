7083bf418fda555698bb90bb20fbe2526cabce2a
Syed Hussain Ather <shussainather@gmail.com>
2019-07-11 20:02:59 -0400

    parse

--- mech/fallingball.py
+++ mech/fallingball.py
@@ -10,3 +10,10 @@ ball dropped from rest close to the surface of Earth . The
 program depends on the function VelocityLinearDrag to execute
 the Euler method.
 """
+
+t = 0
+g = 9.8
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--filename", dest="filename", action="store", help="output filename", default=junk.dat")
