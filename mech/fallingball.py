67a4ea21189b7d661bc6ffae81e350276dac8b47
Syed Hussain Ather <shussainather@gmail.com>
2019-07-11 20:02:02 -0400

    import

--- mech/fallingball.py
+++ mech/fallingball.py
@@ -1,4 +1,8 @@
-
+import sys, EulerFreeFall
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import datetime
 
 """
 This program uses the Euler method to solve for the motion of a
