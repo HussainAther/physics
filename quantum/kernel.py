3c047a072c748fc2564941f9b9c1c5f1242789dc
Syed Hussain Ather <shussainather@gmail.com>
2019-08-05 13:26:07 -0400

    data

--- quantum/kernel.py
+++ quantum/kernel.py
@@ -9,3 +9,7 @@ Kernel method for spatial mapping.
 qvm_server, quilc_server, fc = init_qvm_and_quilc("")
 qc == get_qc("4q-qvm", connection=fc)
 
+# Data
+training_set = [[0, 1], [0.78861006, 0.61489363]]
+labels = [0, 1]
+test_set = [[-0.549, 0.836], [0.053 , 0.999]]
