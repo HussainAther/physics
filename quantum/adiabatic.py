de0e08fa20b57845a5de5a68555c9a0e911300d5
Syed Hussain Ather <shussainather@gmail.com>
2019-08-05 12:50:48 -0400

    annealing

--- quantum/adiabatic.py
+++ quantum/adiabatic.py
@@ -1,3 +1,25 @@
+import numpy as np
+import dimod
+
 """
-Adiabatic quantum computing.
+Adiabatic quantum computing and adiabatic theorem
 """
+
+np.set_printoptions(precision=3, suppress=True)
+
+X = np.array([[0, 1], [1, 0]])
+IX = np.kron(np.eye(2), X)
+XI = np.kron(X, np.eye(2))
+H_0 = - (IX + XI)
+λ, v = np.linalg.eigh(H_0)
+print("Eigenvalues:", λ)
+print("Eigenstate for lowest eigenvalue", v[:, 0])
+
+# Annealing
+J = {(0, 1): 1.0, (1, 2): -1.0}
+h = {0:0, 1:0, 2:0}
+model = dimod.BinaryQuadraticModel(h, J, 0.0, dimod.SPIN)
+sampler = dimod.SimulatedAnnealingSampler()
+response = sampler.sample(model, num_reads=10)
+print("Energy of samples:")
+print([solution.energy for solution in response.data()])
