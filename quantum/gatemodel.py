dcf57581fc79e6046d201e72f0c2c5ff26bccac4
Syed Hussain Ather <shussainather@gmail.com>
2019-08-05 12:43:22 -0400

    start

--- quantum/gatemodel.py
+++ quantum/gatemodel.py
@@ -2,7 +2,19 @@ import numpy as np
 
 from pyquil import Program, get_qc
 from pyquil.api import WavefunctionSimulator
+from pyquil.gates import CNOT, H
+from foresttools import init_qvm_and_quilc
 
 """
 Gate-model in quantum computing.
 """
+
+np.set_printoptions(precision=3, suppress=True)
+qvm_server, quilc_server, fc = init_qvm_and_quilc("/")
+qc = get_qc('2q-qvm', connection=fc)
+
+wf_sim = WavefunctionSimulator(connection=fc)
+circuit = Program()
+circuit += H(0)
+circuit += CNOT(0, 1)
+plot_circuit(circuit)
