7adc2c9519ad5e05fc28b1c188c793bf4b6dcb68
Syed Hussain Ather <shussainather@gmail.com>
2019-08-05 12:47:13 -0400

    start

--- quantum/qiskit.py
+++ quantum/qiskit.py
@@ -1,3 +1,15 @@
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit import execute
+from qiskit import BasicAer as Aer
+from qiskit.tools.visualization import circuit_drawer, plot_histogram
+
 """
 Noisy quantum computers.
 """
+
+q = QuantumRegister(2)
+c = ClassicalRegister(2)
+circuit = QuantumCircuit(q, c)
+circuit.h(q[0])
+circuit.cx(q[0], q[1])
+circuit_drawer(circuit)
