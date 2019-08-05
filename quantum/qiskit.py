0dde497a25488b4cc81fb5b48d01c76819d3989a
Syed Hussain Ather <shussainather@gmail.com>
2019-08-05 12:48:36 -0400

    compilie

--- quantum/qiskit.py
+++ quantum/qiskit.py
@@ -1,6 +1,7 @@
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit import execute
 from qiskit import BasicAer as Aer
+from qiskit.compiler import assemble
 from qiskit.tools.visualization import circuit_drawer, plot_histogram
 from foresttools import plot_histogram
 
@@ -19,3 +20,5 @@ circuit_drawer(circuit)
 backend = Aer.get_backend("qasm_simulator")
 job = execute(circuit, backend, shots=100)
 plot_histogram(job.result().get_counts(circuit))
+compiled_circuit = assemble(circuit, backend)
+compiled_circuit.as_dict()["experiments"][0]["instructions"]
