c86a6c78f40fcf68fc7d409e34db14d46806b5b6
Syed Hussain Ather <shussainather@gmail.com>
2019-08-05 13:13:57 -0400

    init

--- quantum/matrixinversion.py
+++ quantum/matrixinversion.py
@@ -2,8 +2,12 @@ import numpy as np
 import scipy.linalg
 
 from grove.alpha.phaseestimation.phase_estimation import controlled
-from pyquil import Program, get_qc
+from pyquil import Program, get_qc, init_qvm_and_quilc
 
 """
 Harrow-Hassidim-Lloyd (HHL) algorithm.
 """
+
+qvm_server, quilc_server, fc = init_qvm_and_quilc("")
+qc = get_qc("6q-qvm", connection=fc)
+pi = np.pi
