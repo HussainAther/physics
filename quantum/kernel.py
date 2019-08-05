09e73626cde0369109254101a20989a2b3b9c1d2
Syed Hussain Ather <shussainather@gmail.com>
2019-08-05 13:26:39 -0400

    angle

--- quantum/kernel.py
+++ quantum/kernel.py
@@ -13,3 +13,9 @@ qc == get_qc("4q-qvm", connection=fc)
 training_set = [[0, 1], [0.78861006, 0.61489363]]
 labels = [0, 1]
 test_set = [[-0.549, 0.836], [0.053 , 0.999]]
+
+def get_angle(amplitude_0):
+    """
+    Solve quantum equation to get angle.
+    """
+    return 2*np.arccos(amplitude_0)
