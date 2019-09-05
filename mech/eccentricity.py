cd603b452b587777bd3379cf404fa70aaf2d3bd4
Syed Hussain Ather <shussainather@gmail.com>
2019-09-05 12:16:08 -0700

    argv

--- mech/eccentricity.py
+++ mech/eccentricity.py
@@ -2,5 +2,8 @@ import sys
 
 """
 Calculate eccentricity of an orbit from a list of radii.
+Use the argument of input after running this program to 
+give the list of radii.
 """
 
+distlist = sys.argv[1]
