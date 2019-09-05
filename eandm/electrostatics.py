73bc84d490dd0b528501cda30c4ca52d17fc0dff
Syed Hussain Ather <shussainather@gmail.com>
2019-09-04 17:05:52 -0700

    import

--- eandm/electrostatics.py
+++ eandm/electrostatics.py
@@ -1,3 +1,11 @@
+import functools
+import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib import rcParams
+from scipy.integrate import ode
+from scipy.interpolate import splrep, splev
+
 """
 Electrostatics (electrostatic) functionality
 """
