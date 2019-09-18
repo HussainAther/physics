f6e222591dd54e7a32cf4e66f0ecd3c00b106c9b
Syed Hussain Ather <shussainather@gmail.com>
2019-09-18 16:55:36 -0700

    init var

--- statmech/fir.py
+++ statmech/fir.py
@@ -9,3 +9,19 @@ from scipy.ndimage import convolve1d
 """
 Finite impulse response (FIR) filter on input signal using numpy as scipy.
 """
+
+m = 1
+n = 2 ** 18
+x = np.random.random(size=(m, n))
+
+conv_time = []
+npconv_time = []
+fftconv_time = []
+conv1d_time = []
+lfilt_time = []
+
+diff_list = []
+diff2_list = []
+diff3_list = []
+
+ntaps_list = 2 ** np.arange(2, 14)
