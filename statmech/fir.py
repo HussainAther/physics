bb6bcefff1070dc7bab501d6bfd42fc707ae2161
Syed Hussain Ather <shussainather@gmail.com>
2019-09-18 16:54:34 -0700

    import

--- statmech/fir.py
+++ statmech/fir.py
@@ -1,3 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from numpy import convolve as np_convolve
+from scipy.signal import fftconvolve, lfilter, firwin
+from scipy.signal import convolve as sig_convolve
+from scipy.ndimage import convolve1d
+
 """
 Finite impulse response (FIR) filter on input signal using numpy as scipy.
 """
