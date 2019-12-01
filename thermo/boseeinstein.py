6f1265607f02301302eab3208d8e682b0899ac58
Syed Hussain Ather <shussainather@gmail.com>
2019-12-01 13:37:23 -0800

    import

--- thermo/boseeinstein.py
+++ thermo/boseeinstein.py
@@ -1,3 +1,10 @@
+import matplotlib
+import matplotlib.pyplot as plt
+
+from matplotlib import animation
+from numpy import exp, pi, arange, meshgrid, sqrt, linspace
+from numpy.fft import fft2, ifft2, fftshift
+
 """
 Bose-Einstein condensate (bose einstein BEC) for evolving Gross-Pitaevskii
 equation (gross pitaevskii gpe). 
