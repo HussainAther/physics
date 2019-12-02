ffac67430c02c4e374115c670f28bb12461b7592
Syed Hussain Ather <shussainather@gmail.com>
2019-12-02 08:27:31 -0800

    import

--- statmech/wolff.py
+++ statmech/wolff.py
@@ -1,3 +1,6 @@
+from __future__ import division, print_function
+from random import random
+
 """
 Wolff algorithm for Monte Carlo simulation of the Ising model in which the unit to be flipped is not a single spin, as in the heat bath or Metropolis (metropolis-hastings hastings) algorithms, but a cluster of them.
 """
