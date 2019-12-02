e0770fcbfd76601a12bed8f8f85da6c6f486b2b3
Syed Hussain Ather <shussainather@gmail.com>
2019-12-02 08:36:42 -0800

    indent comment

--- statmech/wolff.py
+++ statmech/wolff.py
@@ -6,7 +6,9 @@ from visual import divmod, mod, rate
 from visual.graph import box, color, gdots 
 
 """
-Wolff algorithm for Monte Carlo simulation of the Ising model in which the unit to be flipped is not a single spin, as in the heat bath or Metropolis (metropolis-hastings hastings) algorithms, but a cluster of them.
+Wolff algorithm for Monte Carlo simulation of the Ising model in which 
+the unit to be flipped is not a single spin, as in the heat bath or 
+Metropolis (metropolis-hastings hastings) algorithms, but a cluster of them.
 """
 
 N = 64 #inputs
