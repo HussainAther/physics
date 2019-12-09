172bd431891ddfe6ed3bc29d85946f61597b0c06
Syed Hussain Ather <shussainather@gmail.com>
2019-12-09 00:34:29 -0800

    init

--- fluid/brownian.py
+++ fluid/brownian.py
@@ -12,5 +12,14 @@ class gbm():
     
     GBM_t = GBM_0 * exp((mu -.5*sigma^2)*t + sigma*W_t)
     """
-
-
+    def __init__(self, 
+                 mu, 
+                 sigma, 
+                 startPosition = 1, 
+                 startTime = 0, 
+                 endPosition = None, 
+                 endTime = None):
+        super(GBM_process,self).__init__(startTime, startPosition, endTime, endPosition)
+        self.mu = mu
+        self.sigma = sigma
+        self.Nor = stats.norm(0,1)
