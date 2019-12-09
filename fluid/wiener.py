4217ab4bfffeeab8b53927545968662490858827
Syed Hussain Ather <shussainather@gmail.com>
2019-12-09 00:39:04 -0800

    init

--- fluid/wiener.py
+++ fluid/wiener.py
@@ -1,3 +1,9 @@
-"""
-Wiener process implementation
-"""
+
+
+class wienerprocess():
+    """
+    Wiener process implementation as a special case of brownian motion
+    with 0 drift and variance of t.
+    dW_t = mu*dt + sigma*dB_t
+    W_t ~ N*(mu*t, sigma**2t)
+    """
