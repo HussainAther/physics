cacbafef8c102539eddf466005476c05f62a3fc3
Syed Hussain Ather <shussainather@gmail.com>
2019-12-09 00:27:56 -0800

    class

--- fluid/brownian.py
+++ fluid/brownian.py
@@ -1,12 +1,16 @@
 import numpy as np
 
-"""
-Brownian motion process by stochastic differential
-equation (SDE)
- 
-dGM_t = mu*GMB_tdt + sigma*GM_t*dW_t
 
-with solution 
+class gbm():
+    """
+    Geometric Brownian motion process by stochastic 
+    differential equation (SDE)
+     
+    dGM_t = mu*GMB_tdt + sigma*GM_t*dW_t
+    
+    with solution 
+    
+    GBM_t = GBM_0 * exp((mu -.5*sigma^2)*t + sigma*W_t)
+    """
 
-GBM_t = GBM_0 * exp((mu -.5*sigma^2)*t + sigma*W_t)
-"""
+
