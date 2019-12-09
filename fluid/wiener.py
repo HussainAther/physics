724bd28dd8db46ccdd86edc6cfe3c0a70f8a024d
Syed Hussain Ather <shussainather@gmail.com>
2019-12-09 00:40:49 -0800

    init var

--- fluid/wiener.py
+++ fluid/wiener.py
@@ -7,3 +7,14 @@ class wienerprocess():
     dW_t = mu*dt + sigma*dB_t
     W_t ~ N*(mu*t, sigma**2t)
     """
+    def __init__(self,  
+                 mu, 
+                 sigma, 
+                 startTime = 0, 
+                 startPosition = 0, 
+                 endPosition = None, 
+                 endTime = None):
+        super(wienerprocess,self).__init__(startTime, startPosition, endTime, endPosition)
+        self.mu = mu
+        self.sigma = sigma
+        self.Nor = stats.norm()
