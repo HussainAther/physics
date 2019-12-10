8f0d2d86906f07ef3b3f05f950883b5561c6dc00
Syed Hussain Ather <shussainather@gmail.com>
2019-12-09 18:22:02 -0800

    init

---
+++ fluid/process/ou.py
@@ -0,0 +1,8 @@
+"""
+Orstein-Uhlenbeck process is a stochastic process that
+uses a mean reverting model. It's also called the Vasicek model.
+It uses the stochastic differential equation:
+
+dOU_t = theta * (m - OU_t) * dt + sigma * dB_t 
+
+"""
