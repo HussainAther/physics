62881fc36a4217a391e55dabf4c00c0b3b8d7ed9
Syed Hussain Ather <shussainather@gmail.com>
2019-12-09 19:36:41 -0800

    init out

--- fluid/process/ou.py
+++ fluid/process/ou.py
@@ -9,3 +9,17 @@ class ouprocess(p):
     
     dOU_t = theta * (m - OU_t) * dt + sigma * dB_t 
     """
+    def __init__(self, 
+                 theta, 
+                 mu, 
+                 sigma, 
+                 startTime = 0, 
+                 startPosition = 0, 
+                 endPosition = None, 
+                 endTime = None):
+        assert sigma > 0 and theta > 0, "theta > 0 and sigma > 0."
+        super(OU_process, self).__init__(startTime, startPosition, endTime, endPosition)
+        self.theta = theta
+        self.mu = mu
+        self.sigma = sigma
+        self.Normal = stats.norm()
