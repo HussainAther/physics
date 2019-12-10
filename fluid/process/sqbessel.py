2a6a70039e833bfd12cdd64e82c31c161d13d748
Syed Hussain Ather <shussainather@gmail.com>
2019-12-09 19:58:12 -0800

    init var

--- fluid/process/sqbessel.py
+++ fluid/process/sqbessel.py
@@ -8,3 +8,24 @@ class SqBessel_process(p):
     Based on R.N. Makarov and D. Glew's research on simulating squared bessel process. See "Exact
     Simulation of Bessel Diffusions", 2011.
     """
+    def __init__(self, 
+                 lambda_0, 
+                 nu, 
+                 startTime = 0, 
+                 startPosition = 1, 
+                 endTime = None, 
+                 endPosition = None):
+            super(SqBessel_process, self).__init__(startTime, startPosition, endTime, endPosition)
+            try:
+                self.endPosition = 4.0/parameters["nu"]**2*self.endPosition
+                self.x_T = self.endPosition
+            except:
+                pass
+            self.x_0 = 4.0/nu**2*self.startPosition
+            self.nu = nu
+            self.lambda0 = lambda_0
+            self.mu = 2*float(self.lambda0)/(self.nu*self.nu)-1
+            self.Poi = stats.poisson
+            self.Gamma = stats.gamma
+            self.Nor = stats.norm
+            self.InGamma = IncompleteGamma
