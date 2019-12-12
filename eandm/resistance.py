32f8a094cd5055e0ce9a426b66d4340fe997f15f
Syed Hussain Ather <shussainather@gmail.com>
2019-12-12 01:13:06 -0800

    resistor class

--- eandm/resistance.py
+++ eandm/resistance.py
@@ -2,3 +2,10 @@
 Calculate the resistance for a set of resistors
 following electrical circuit theory.
 """
+
+class resistor:
+    def __init__(self,
+                 resistance,
+                 a=None,
+                 b=None,
+                 symbol="r"):
