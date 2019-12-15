ec52a10338e40e8e367e3e256088135bb0b3593a
Syed Hussain Ather <shussainather@gmail.com>
2019-12-15 03:09:24 -0800

    newcolors

--- fluid/brownian.py
+++ fluid/brownian.py
@@ -13,29 +13,26 @@ T = 1 # start time
 tstep = .0002 # time step
 sigma = .4 # percent voltality
 mu = 1 # percent drift
-
-# standard colors
-colors = [(214, 27, 31), 
-          (148, 104, 189),
-          (229, 109, 0),
-          (41, 127, 214),
-          (217, 119, 194),
-          (44, 160, 44),
-          (227, 119, 194),
-          (72, 17, 121),
-          (196, 156, 148)]
-
-# Scale the colors to a [0, 1] range
-for i in range(len(colors)):
-    r, g, b = colors[i]
-    colors[i] = (r/255, g/255, b/255)
+NumSimulation = 6
+colors = ["b", "g", "r", "c", "m", "k"]
 
 # Plot.
 plt.figure(figsize=(12,12))
 
-Steps = round(T/tstep) # steps in years
-S = np.zeros([NumSimulation, Steps], dtype=float)
+Steps = int(round(T/tstep)) # steps in years
+S = np.zeros([NumSimulation, Steps])
 x = range(0, int(Steps))
 
 for j in range(0, NumSimulation):
     S[j,0] = Sinit
+    for i in x[:-1]:
+        S[j, i+1] = S[j,i]+S[j,i]*(mu-.5*pow(sigma, 2))*tstep+sigma*S[j,i]*np.sqrt(tstep)*standard_normal()
+
+    plt.plot(x, S[j], linewidth=2, color=colors[j])
+
+plt.title("%d Brownian motion simulations using %d Steps, \n$\sigma$=%.6f $\mu$=%.6f$S_O$=%.6f" % (int(NumSimulation), int(Steps), sigma, mu, Sinit), fontsize=18)
+plt.xlabel("Steps", fontsize=16)
+plt.grid(True)
+plt.ylabel("Stock price", fontsize=16)
+plt.ylim(0, 90)
+plt.show()
