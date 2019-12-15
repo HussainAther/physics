e57748f63ae5338ddac516de5fe4c695d7ba0983
Syed Hussain Ather <shussainather@gmail.com>
2019-12-15 02:47:07 -0800

    steps

--- fluid/brownian.py
+++ fluid/brownian.py
@@ -14,6 +14,7 @@ tstep = .0002 # time step
 sigma = .4 # percent voltality
 mu = 1 # percent drift
 
+# standard colors
 colors = [(214, 27, 31), 
           (148, 104, 189),
           (229, 109, 0),
@@ -23,3 +24,18 @@ colors = [(214, 27, 31),
           (227, 119, 194),
           (72, 17, 121),
           (196, 156, 148)]
+
+# Scale the colors to a [0, 1] range
+for i in range(len(colors)):
+    r, g, b = colors[i]
+    colors[i] = (r/255, g/255, b/255)
+
+# Plot.
+plt.figure(figsize=(12,12))
+
+Steps = round(T/tstep) # steps in years
+S = np.zeros([NumSimulation, Steps], dtype=float)
+x = range(0, int(Steps))
+
+for j in range(0, NumSimulation):
+    S[j,0] = Sinit
