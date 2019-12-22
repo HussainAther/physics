4aff98139932f6989443611354e94098686308a7
Syed Hussain Ather <shussainather@gmail.com>
2019-12-21 22:52:40 -0800

    comment

--- fluid/browniantree.py
+++ fluid/browniantree.py
@@ -42,7 +42,7 @@ class Particle(pygame.sprite.Sprite):
         self.surface = surface
         self.accelerate(vector)
         self.add(freeParticles)
-        self.rect = pygame.Rect(location[0], location[1], SIZE, SIZE)
+        self.rect = pygame.Rect(location[0], location[1], size, size)
         self.surface.fill(color, self.rect)
 
     def onEdge(self):
@@ -50,10 +50,52 @@ class Particle(pygame.sprite.Sprite):
         The edge where the vectors meet.
         """
         if self.rect.left <= 0:
+            """
+            If the left side of the rectangle of the particle
+            is to the left of the x=0 line, we take the absolute
+            value of its velocity in the x-direction in determining 
+            the new vector. 
+            """
             self.vector = (abs(self.vector[0]), self.vector[1])
         elif self.rect.top <= 0:
             self.vector = (self.vector[0], abs(self.vector[1]))
         elif self.rect.right >= windowsize:
             self.vector = (-abs(self.vector[0]), self.vector[1])
-        elif self.rect.bottom >= WINDOWSIZE:
+        elif self.rect.bottom >= windowsize:
+            """
+            When a particle hits the bottom of the window, we push
+            it back upward.
+            """
             self.vector = (self.vector[0], -abs(self.vector[1]))
+
+    def update(self):
+        """
+        Update each particle's velocity and location.
+        """
+        if freeParticles in self.groups():
+            self.surface.fill((0,0,0), self.rect)
+            """
+            Fill the particles with white color when they're 
+            free.
+            """
+            self.remove(freeParticles)
+            if pygame.sprite.spritecollideany(self, freeParticles):
+                """
+                When a collision occurs, ecah particle experiences
+                an acceleration.
+                """
+                self.accelerate((randint(-maxspeed, maxspeed), 
+                                 randint(-maxspeed, maxspeed)))
+                self.add(freeParticles)
+            elif pygame.sprite.spritecollideany(self, tree):
+                """
+                Stop the particles from moving when they collide
+                with the tree.
+                """
+                self.stop()
+            else:
+                """
+                If they haven't collided with the tree, they're
+                still free particles.
+                """
+                self.add(freeParticles)
