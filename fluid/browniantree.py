96534ee52daf4e289217c15bd5875e06ed0badca
Syed Hussain Ather <shussainather@gmail.com>
2019-12-21 22:30:29 -0800

    set it up

--- fluid/browniantree.py
+++ fluid/browniantree.py
@@ -6,7 +6,7 @@ from pygame.locals import
 from random import randint
 
 """
-Brown tree
+Brownian tree with pygame
 
 Make it beautiful.
 """
@@ -17,3 +17,13 @@ color = (45, 90, 45)
 windowsize = 400
 timetick = 1
 maxpart = 50
+
+# Get the sprites.
+freeParticles = pygame.sprite.Group()
+tree = pygame.sprite.Group()
+
+# Set up the display. 
+window = pygame.display.set_mode((windowsize, windowsize))
+pygame.display.set_caption("Brownian Tree")
+ 
+screen = pygame.display.get_surface()
