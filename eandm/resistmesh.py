1eeeb94a7f308bf334a2b80924962f0d3fd9f46b
Syed Hussain Ather <shussainather@gmail.com>
2019-12-27 00:01:04 -0800

    class fixed

--- eandm/resistmesh.py
+++ eandm/resistmesh.py
@@ -3,4 +3,9 @@ With a 10x10 grid of resistors (each 1 ohm), find the resistance
 between two resistors separated by distance (from here: https://xkcd.com/356/).
 """
 
-tol = 1e-40 # tolerance for the difference between the two points.
+tol = 1e-40 # tolerance for the difference between the two points
+
+class Fixed:
+   free = 0
+   a = 1
+   b = 2
