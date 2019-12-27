616ec38dbf03cfe523f79ebf30939670f10404be
Syed Hussain Ather <shussainather@gmail.com>
2019-12-26 23:29:08 -0800

    tol

--- eandm/resistmesh.py
+++ eandm/resistmesh.py
@@ -2,3 +2,5 @@
 With a 10x10 grid of resistors (each 1 ohm), find the resistance
 between two resistors separated by distance (from here: https://xkcd.com/356/).
 """
+
+tol = 1e-40 # tolerance for the difference between the two points.
