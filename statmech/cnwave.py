cac0770286c82816cffcb8b2966406c0a652e2c9
Syed Hussain Ather <shussainather@gmail.com>
2020-02-15 16:02:16 -0500

    init function

--- statmech/cnwave.py
+++ statmech/cnwave.py
@@ -10,3 +10,16 @@ Convert the eqution to first-order with
 dphi(x,t)/dt - pi(xi,t) = 0
 dphi(xi,t)/dt - d^2phi(x,t)/dx^2 + F(phi) = 0
 """
+
+class cnwaveeq:
+    """
+    Methods for the conversion shown above.
+    """
+    def __init__(self, F, dF, args, xmin, xmax, Nx):
+       """
+       F(phi, args) = is a user-specified function returning F(phi) 
+       dF(phi, args) = is a user-specified function returning dF/dphi(phi)
+       xmin, xmax define the spatial integration domain
+       Nx = is the number of points, so dx=(xmax-xmin)/(N-1)
+       All initial data set to zero, t set to 0.
+       """
