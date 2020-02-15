c4dc0757111809f32141f440d4c8ae3143c13f1f
Syed Hussain Ather <shussainather@gmail.com>
2020-02-15 15:58:33 -0500

    write out the eq

--- statmech/cnwave.py
+++ statmech/cnwave.py
@@ -1,5 +1,8 @@
+import numpy as np
+
 """
-Crank-Nicholson scheme to solve a 2D nonlinear wave equation
+Crank-Nicholson scheme to solve a 2D nonlinear wave equation using methods
+from nonlinear dynamics
 
-
+-d^2phi(x,t)/dt^2 + d^2 phi(x,t)/dx^2 = F(phi) 
 """
