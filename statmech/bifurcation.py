b3c1af1b64729161518c098a7725b989742c4eaf
Syed Hussain Ather <shussainather@gmail.com>
2020-02-14 22:04:44 -0500

    equil eq's

--- statmech/bifurcation.py
+++ statmech/bifurcation.py
@@ -4,3 +4,15 @@ import numpy as np
 """
 Bifurcations are transitions between dynamical states used in nonlinear dynamics.
 """
+
+def xeq1(r):
+    """
+    Stable equilibrium
+    """
+    return np.sqrt(r)
+
+def xeq2(r):
+    """
+    Unstable equilibrium
+    """
+    return np.sqrt(r)
