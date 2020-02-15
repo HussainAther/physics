20edb674c14023a36e98439156289deae7890b04
Syed Hussain Ather <shussainather@gmail.com>
2020-02-15 15:59:35 -0500

    convert to first

--- statmech/cnwave.py
+++ statmech/cnwave.py
@@ -5,4 +5,8 @@ Crank-Nicholson scheme to solve a 2D nonlinear wave equation using methods
 from nonlinear dynamics
 
 -d^2phi(x,t)/dt^2 + d^2 phi(x,t)/dx^2 = F(phi) 
+
+Convert the eqution to first-order with
+dphi(x,t)/dt - pi(xi,t) = 0
+dphi(xi,t)/dt - d^2phi(x,t)/dx^2 + F(phi) = 0
 """
