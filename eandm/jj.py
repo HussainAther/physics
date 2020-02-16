5056ef7587086c0a56403acd1beec7c1b846e223
Syed Hussain Ather <shussainather@gmail.com>
2020-02-15 22:34:05 -0500

    jj plots

--- eandm/jj.py
+++ eandm/jj.py
@@ -6,3 +6,22 @@ from scipy.integrate import odeint
 """
 Josephson junction limit cycle (JJ) (Josephson-junction)
 """
+
+fig = plt.figure()
+
+bj = 1.2
+tmax = 100
+kappa = 1.4
+
+def jj_ode(x, t):
+    return [x[1], kappa - bj*x[1] - np.sin(x[0])]
+
+time = np.arange(0, tmax, 0.1)
+x0=[0.1,0.1]
+xs = odeint(jj_ode, x0, time)
+imgplot = plt.plot(np.sin(xs[:, 0]), xs[:, 1], "r-")
+
+plt.xlabel(r"$\sin(\phi)$", fontsize=15)
+plt.ylabel(r"$\Omega$", fontsize=15)
+plt.tick_params(labelsize=15)
+plt.show()
