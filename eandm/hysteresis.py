c7f5048e47900e1aa8ceae22f40b2eafb7a57658
Syed Hussain Ather <shussainather@gmail.com>
2020-02-15 22:45:46 -0500

    plot

--- eandm/hysteresis.py
+++ eandm/hysteresis.py
@@ -18,3 +18,13 @@ def memristor(X, t):
     dwdt = ((eta * (1 - (2*w - 1) ** (2*p)) * np.sin(2*np.pi * t/T))
            / (Roff - (Roff - Ron) * w))
     return dwdt
+
+X = odeint(memristor, [w0], t, rtol=1e-12)
+w = X[:, 0]
+
+plt.plot(np.sin(2*np.pi * t/T), np.sin(2*np.pi * t/T)
+        / (Roff - (Roff - Ron) * X[:, 0]), "b")
+plt.xlabel("voltage", fontsize=15)
+plt.ylabel("current", fontsize=15)
+plt.tick_params(labelsize=15)
+plt.show()
