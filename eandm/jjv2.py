b842e9921bd08caae4fbc25377da47269d1e2b2f
Syed Hussain Ather <shussainather@gmail.com>
2020-02-15 22:41:24 -0500

    import

--- eandm/jjv2.py
+++ eandm/jjv2.py
@@ -1,3 +1,8 @@
+from matplotlib import pyplot as plt
+from matplotlib.animation import ArtistAnimation
+import numpy as np
+from scipy.integrate import odeint
+
 """
 Josephson junction limit cycle (JJ) (Josephson-junction) animation animated
 """
