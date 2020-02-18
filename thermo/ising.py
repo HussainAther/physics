da1cc6c3148004f8598e105a014d5041d3438ed0
Syed Hussain Ather <shussainather@gmail.com>
2020-02-17 22:19:41 -0500

    class

--- thermo/ising.py
+++ thermo/ising.py
@@ -3,3 +3,14 @@ import numpy as np
 """
 Ising model lattice encapsulation
 """
+
+class IsingLattice:
+    """
+    Explore a simple nearest-neighbor 2D Ising model.
+    This is an NxN periodic lattice in which each site
+    has a particle with spin S (+/- 1). You can write the
+    energy of the system as 
+    E = -J Sum_{i,j} [S_{i,j}*(S_{I+1,j}+S_{i,j+1})]
+        -H Sum_{i,j} [S_{i,j}]
+    
+    """
